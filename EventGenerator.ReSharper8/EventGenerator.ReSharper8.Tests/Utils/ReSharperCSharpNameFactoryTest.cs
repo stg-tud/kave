bad3725ed5f20b9aa14cfeecffad6f5d7e9684a3
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2013-11-07 10:44:01 +0100

    began to write tests

--- EventGenerator.ReSharper8/EventGenerator.ReSharper8.Tests/Utils/ReSharperCSharpNameFactoryTest.cs
+++ EventGenerator.ReSharper8/EventGenerator.ReSharper8.Tests/Utils/ReSharperCSharpNameFactoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.ExtensionsAPI.Caches2;
 using JetBrains.ReSharper.Psi.Resources;
+using Moq;
 using NUnit.Framework;
 using KaVE.EventGenerator.ReSharper8.Utils;
 
@@ -12,7 +13,12 @@ namespace KaVE.EventGenerator.ReSharper8.Tests.Utils
         [Test]
         public void ShouldTranslateNamespaceToName()
         {
-            
+            var nsMock = new Mock<INamespace>();
+            nsMock.Setup(ns => ns.QualifiedName).Returns("My.Test.Namespace");
+
+            var namespaceName = nsMock.Object.GetName();
+
+            Assert.AreEqual("My.Test.Namespace", namespaceName.Identifier);
         }
     }
 }
