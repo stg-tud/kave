5602bacbd30075804bf006b92e38f8afe2bc4030
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2013-11-14 13:42:34 +0100

    tested type parameters

--- EventGenerator.ReSharper8/EventGenerator.ReSharper8.Tests/Utils/ReSharperMockUtils.cs
+++ EventGenerator.ReSharper8/EventGenerator.ReSharper8.Tests/Utils/ReSharperMockUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using JetBrains.Application;
 using JetBrains.Metadata.Utils;
 using JetBrains.ProjectModel;
@@ -7,7 +8,10 @@ using JetBrains.ProjectModel.Model2.Assemblies.Interfaces;
 using JetBrains.ProjectModel.Properties;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.DeclaredElements;
+using JetBrains.ReSharper.Psi.Impl.Resolve;
 using JetBrains.ReSharper.Psi.Modules;
+using JetBrains.ReSharper.Psi.Resolve;
 using JetBrains.Util;
 using KaVE.Model.Names.CSharp;
 using Moq;
@@ -19,11 +23,36 @@ namespace KaVE.EventGenerator.ReSharper8.Tests.Utils
     {
         internal static IType MockIType(string fqnOrAlias, string assemblyName, string assemblyVersion)
         {
+            return MockIType(fqnOrAlias, EmptySubstitution.INSTANCE, assemblyName, assemblyVersion);
+        }
+
+        public static IType MockIType(string fqnOrAlias, IEnumerable<KeyValuePair<string, IType>> typeParameters, string assemblyName, string assemblyVersion)
+        {
+            var parameters = new List<ITypeParameter>();
+            var parameterSubstitutes = new List<IType>();
+            foreach (var typeParameter in typeParameters)
+            {
+                var mockTypeParameter = new Mock<ITypeParameter>();
+                mockTypeParameter.Setup(tp => tp.ShortName).Returns(typeParameter.Key);
+                parameters.Add(mockTypeParameter.Object);
+                parameterSubstitutes.Add(typeParameter.Value);
+            }
+            return MockIType(fqnOrAlias, new SubstitutionImpl(parameters, parameterSubstitutes), assemblyName, assemblyVersion);
+        }
+
+        private static IType MockIType(string fqnOrAlias,
+            ISubstitution substitution,
+            string assemblyName,
+            string assemblyVersion)
+        {
             var typeMock = new Mock<IDeclaredType>();
             var fqn = CSharpNameUtils.GetFullTypeNameFromTypeAlias(fqnOrAlias);
             typeMock.Setup(t => t.GetTypeElement()).Returns(MockTypeElement(fqn, assemblyName, assemblyVersion));
             typeMock.Setup(t => t.GetLongPresentableName(CSharpLanguage.Instance)).Returns(fqnOrAlias);
             typeMock.Setup(t => t.Assembly).Returns(MockAssemblyInfo(assemblyName, assemblyVersion));
+            var mockResolveResult = new Mock<IResolveResult>();
+            mockResolveResult.Setup(rr => rr.Substitution).Returns(substitution);
+            typeMock.Setup(t => t.Resolve()).Returns(mockResolveResult.Object);
             return typeMock.Object;
         }
 
@@ -34,6 +63,11 @@ namespace KaVE.EventGenerator.ReSharper8.Tests.Utils
 
         internal static ITypeElement MockTypeElement(string fqn, IModule module)
         {
+            if (fqn.Contains("[["))
+            {
+                fqn = fqn.Substring(0, fqn.IndexOf("[[", System.StringComparison.Ordinal));
+            }
+
             var clrMock = new Mock<IClrTypeName>();
             clrMock.Setup(clr => clr.FullName).Returns(fqn);
 
@@ -68,7 +102,8 @@ namespace KaVE.EventGenerator.ReSharper8.Tests.Utils
         internal static IProject MockProject(string name, string version)
         {
             var mockProject = MockProjectImpl(name);
-            mockProject.OutputAssembly = MockAssembly(name, version);
+            var mockAssemblyInfo = new OutputAssemblyInfo(Guid.NewGuid(), MockAssemblyInfo(name, version), null);
+            mockProject.OutputAssemblyInfo = mockAssemblyInfo;
             return mockProject;
         }
 
--- EventGenerator.ReSharper8/EventGenerator.ReSharper8.Tests/Utils/ReSharperTypeNameFactoryTest.cs
+++ EventGenerator.ReSharper8/EventGenerator.ReSharper8.Tests/Utils/ReSharperTypeNameFactoryTest.cs
@@ -1,4 +1,6 @@
-﻿using JetBrains.ReSharper.Psi;
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.ReSharper.Psi;
 using KaVE.EventGenerator.ReSharper8.Utils;
 using NUnit.Framework;
 
@@ -7,16 +9,86 @@ namespace KaVE.EventGenerator.ReSharper8.Tests.Utils
     [TestFixture]
     public class ReSharperTypeNameFactoryTest
     {
-        [TestCase("System.String", "mscore", "4.0.0.0", "System.String, mscore, Version=4.0.0.0")]
-        [TestCase("System.Nullable`1[[System.Int32]]", "mscore", "4.0.0.0", "System.Nullable`1[[System.Int32]], mscore, Version=4.0.0.0")]
-        [TestCase("Some.Outer+Inner", "Assembly", "5.4.3.2", "Some.Outer+Inner, Assembly, Version=5.4.3.2")]
-        [TestCase("ValueType[,,]", "A", "9.8.7.6", "ValueType[,,], A, Version=9.8.7.6")]
-        public void ShouldGetNameForIType(string typeFqn, string assemblyName, string assemblyVersion, string identifier)
+        [TestCase("System.String", "mscore", "4.0.0.0", Result = "System.String, mscore, Version=4.0.0.0"),
+         TestCase("Some.Outer+Inner", "Assembly", "5.4.3.2", Result = "Some.Outer+Inner, Assembly, Version=5.4.3.2"),
+         TestCase("ValueType[,,]", "A", "9.8.7.6", Result = "ValueType[,,], A, Version=9.8.7.6")]
+        public string ShouldGetNameForIType(string typeFqn, string assemblyName, string assemblyVersion)
         {
-            // TODO test how generic really look like!
-            var type = ReSharperMockUtils.MockIType(typeFqn, assemblyName, assemblyVersion);
+            return ReSharperMockUtils.MockIType(typeFqn, assemblyName, assemblyVersion).GetName().Identifier;
+        }
 
-            AssertNameIdentifier(type, identifier);
+        public struct GenericTypeCase
+        {
+            private readonly string _fullName;
+            private readonly IDictionary<string, IType> _typeParameters;
+            private readonly string _assemblyName;
+            private readonly string _assemblyVersion;
+            private readonly string _expectedIdentifier;
+
+            public GenericTypeCase(string fullName,
+                IDictionary<string, IType> typeParameters,
+                string assemblyName,
+                string assemblyVersion,
+                string identifier)
+            {
+                _fullName = fullName;
+                _typeParameters = typeParameters;
+                _assemblyName = assemblyName;
+                _assemblyVersion = assemblyVersion;
+                _expectedIdentifier = identifier;
+            }
+
+            public string FullName
+            {
+                get { return _fullName; }
+            }
+
+            public IEnumerable<KeyValuePair<string, IType>> TypeParameters
+            {
+                get { return _typeParameters.AsEnumerable(); }
+            }
+
+            public string AssemblyName
+            {
+                get { return _assemblyName; }
+            }
+
+            public string AssemblyVersion
+            {
+                get { return _assemblyVersion; }
+            }
+
+            public string ExpectedIdentifier
+            {
+                get { return _expectedIdentifier; }
+            }
+        }
+
+        private static readonly GenericTypeCase[] GenericTypeCases =
+        {
+            new GenericTypeCase(
+                "System.Nullable`1",
+                new Dictionary<string, IType> {{"T", ReSharperMockUtils.MockIType("System.Int32", "mscore", "4.0.0.0")}},
+                "mscore",
+                "4.0.0.0",
+                "System.Nullable`1[[T -> System.Int32, mscore, Version=4.0.0.0]], mscore, Version=4.0.0.0"),
+            new GenericTypeCase("System.Collections.IDictionary`2",
+                new Dictionary<string, IType>
+                {
+                    {"TKey", ReSharperMockUtils.MockIType("System.String", "mscore", "1.2.3.4")},
+                    {"TValue", ReSharperMockUtils.MockIType("System.Object", "mscore", "5.6.7.8")}
+                },
+                "mscore",
+                "9.10.11.12",
+                "System.Collections.IDictionary`2[[TKey -> System.String, mscore, Version=1.2.3.4],[TValue -> System.Object, mscore, Version=5.6.7.8]], mscore, Version=9.10.11.12")
+        };
+
+        [TestCaseSource("GenericTypeCases")]
+        public void ShouldGetNameForGenericIType(GenericTypeCase gtc)
+        {
+            var type = ReSharperMockUtils.MockIType(gtc.FullName, gtc.TypeParameters, gtc.AssemblyName, gtc.AssemblyVersion);
+
+            AssertNameIdentifier(type, gtc.ExpectedIdentifier);
         }
 
         private static void AssertNameIdentifier(IType type, string identifier)
@@ -24,6 +96,5 @@ namespace KaVE.EventGenerator.ReSharper8.Tests.Utils
             var name = type.GetName();
             Assert.AreEqual(identifier, name.Identifier);
         }
-        
     }
 }
