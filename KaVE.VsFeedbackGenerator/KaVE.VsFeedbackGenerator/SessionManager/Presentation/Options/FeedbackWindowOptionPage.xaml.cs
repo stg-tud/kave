223e3e5674d9a05d2b94dc1272ccafdd6867767d
Uli <uli.fahrer@googlemail.com>
2013-12-05 18:43:10 +0100

    * Add Optionpage

---
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/Presentation/Options/FeedbackWindowOptionPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using JetBrains.DataFlow;
+using JetBrains.ReSharper.Features.Common.Options;
+using JetBrains.ReSharper.Features.Finding.Resources;
+using JetBrains.UI.CrossFramework;
+using JetBrains.UI.Options;
+
+namespace KaVE.VsFeedbackGenerator.SessionManager.Presentation.Options
+{
+    [OptionsPage(PID, "FeedbackGeneratorOptions", typeof(FeaturesFindingThemedIcons.SearchOptionsPage), ParentId = ToolsPage.PID)]
+    public partial class FeedbackWindowOptionPage : UserControl, IOptionsPage
+    {
+        private const string PID = "FeedbackGenerator.OptionPage";
+
+        public FeedbackWindowOptionPage(Lifetime lifetime, OptionsSettingsSmartContext ctx)
+        {
+            InitializeComponent();
+            /*ctx.SetBinding(lifetime, (FeedbackWindowSettingsResharper s) => s.FeedbackGeneratorNames, NamesCheckBox, CheckBox.IsCheckedProperty);
+            ctx.SetBinding(lifetime, (FeedbackWindowSettingsResharper s) => s.FeedbackGeneratorStartTime, StartTimeCheckBox, CheckBox.IsCheckedProperty);
+            ctx.SetBinding(lifetime, (FeedbackWindowSettingsResharper s) => s.FeedbackGeneratorDuration, DurationCheckBox, CheckBox.IsCheckedProperty);
+            ctx.SetBinding(lifetime, (FeedbackWindowSettingsResharper s) => s.FeedbackGeneratorSessionIDs, SessionIDCheckBox, CheckBox.IsCheckedProperty);*/
+        }
+
+        public bool OnOk()
+        {
+            return true;
+        }
+
+        public bool ValidatePage()
+        {
+            return true;
+        }
+
+        public EitherControl Control
+        {
+            get { return this; }
+        }
+
+        public string Id
+        {
+            get { return PID; }
+        }
+    }
+}
---
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/Presentation/Options/FeedbackWindowSettingsResharper.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Application.Communication;
+using JetBrains.Application.Settings;
+
+namespace KaVE.VsFeedbackGenerator.SessionManager.Presentation.Options
+{
+    [SettingsKey(typeof(InternetSettings), "FeedbackWindow Settings")]
+    class FeedbackWindowSettingsResharper : FeedbackGeneratorSettings
+    {
+        [SettingsEntry("True", "FeedbackGenerator Names")]
+        public new bool FeedbackGeneratorNames
+        {
+            get { return base.Names; }
+            set { base.Names = value; }
+        }
+
+        [SettingsEntry("True", "FeedbackGenerator Duration")]
+        public new bool FeedbackGeneratorDuration
+        {
+            get { return base.Duration;  }
+            set { base.Duration = value;  }
+        }
+
+        [SettingsEntry("True", "FeedbackGenerator StartTime")]
+        public new bool FeedbackGeneratorStartTime
+        {
+            get { return base.StartTime; }
+            set { base.StartTime = value; }
+        }
+
+        [SettingsEntry("True", "FeedbackGenerator SessioIDs")]
+        public new bool FeedbackGeneratorSessionIDs
+        {
+            get { return base.SessionIDs; }
+            set { base.SessionIDs = value; }
+        }
+    }
+}
