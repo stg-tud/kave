1af01b8ff91f81d73d6b3edf774f244e287f06e8
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2013-12-06 17:29:58 +0100

    * removed nuget package from source control * fixed nuget build script

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator-build-nuget-package.bat
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator-build-nuget-package.bat
@@ -1,4 +1,7 @@
-# this picks the first .csproj or .nuspec file in the working directory and builds a .nupkg from it
-# to package a specific project or file, add the .csproj or .nupkg file name after the pack option
-# -NoPackageAnalysis supresses warnings, caused by R# bundles not adhering to the nuget structure conventions
+@echo off
+
+:: this picks the first .csproj or .nuspec file in the working directory and builds a .nupkg from it
+:: to package a specific project or file, add the .csproj or .nupkg file name after the pack option
+:: -NoPackageAnalysis supresses warnings, caused by R# bundles not adhering to the nuget structure conventions
+
 ..\.nuget\nuget pack -Verbosity detailed -NoPackageAnalysis
