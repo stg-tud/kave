6998874885eb281ca7af58181168231089b8898b
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2013-12-06 18:10:23 +0100

    added build time to nuget package name

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator-build-nuget-package.bat
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator-build-nuget-package.bat
@@ -4,4 +4,14 @@
 :: to package a specific project or file, add the .csproj or .nupkg file name after the pack option
 :: -NoPackageAnalysis supresses warnings, caused by R# bundles not adhering to the nuget structure conventions
 
-..\.nuget\nuget pack -Verbosity detailed -NoPackageAnalysis
+set TIME1=%time::=%
+set TIME2=%TIME1:~0,-3%
+::echo %TIME2%
+
+set DATE1=%date:~-4,4%%date:~-7,2%%date:~-10,2%
+::echo %DATE1%
+
+set TIMESTAMP=%DATE1%-%TIME2%
+::echo %TIMESTAMP%
+
+..\.nuget\nuget pack -Verbosity detailed -NoPackageAnalysis -Version 1.0.0-v%TIMESTAMP%
