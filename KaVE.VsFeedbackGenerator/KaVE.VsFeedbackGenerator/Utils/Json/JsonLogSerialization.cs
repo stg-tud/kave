d22e711760d04efb96f5998667bbc171f91c3668
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2013-12-12 11:25:12 +0100

    * fixed assembly-resolution problem in deserialization of log files

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/Utils/Json/JsonLogSerialization.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/Utils/Json/JsonLogSerialization.cs
@@ -1,16 +1,33 @@
 ﻿using System;
-using System.Globalization;
 using System.Reflection;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
+using KaVE.Model.Names;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace KaVE.VsFeedbackGenerator.Utils.Json
 {
     static class JsonLogSerialization
     {
+        static JsonLogSerialization()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+        }
+
+        /// <summary>
+        /// During deserialization of log files, loading the model assembly fails, when model types are used as type
+        /// parameters of type from other assemblies, e.g., in case of lists of model objects.
+        /// </summary>
+        private static Assembly CurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var assemblyName = args.Name;
+            if (assemblyName.StartsWith("KaVE.Model"))
+            {
+                return typeof(IName).Assembly;
+            }
+            return null;
+        }
+
         internal static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
         {
             Converters =
@@ -20,37 +37,9 @@ namespace KaVE.VsFeedbackGenerator.Utils.Json
             Formatting = Formatting.None,
             NullValueHandling = NullValueHandling.Ignore,
             TypeNameHandling = TypeNameHandling.Objects,
-            TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple,
-            Binder = new MyTypeNameBinderForDebugging()
+            TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple
         };
 
         internal static readonly Encoding Encoding = new UTF8Encoding(false);
-    }
-
-    public class MyTypeNameBinderForDebugging : DefaultSerializationBinder
-    {
-        public override Type BindToType(string assemblyName, string typeName)
-        {
-            if (typeName.Contains("[["))
-            {
-                // TODO make this a clean solution!
-
-                // resolution of only the type parameter succeeds
-                //var typeNameStart = typeName.IndexOf("[[", System.StringComparison.Ordinal) + 2;
-                //var typeNameEnd = typeName.IndexOf("]]", System.StringComparison.Ordinal);
-                //var parameterTypeName = typeName.Substring(typeNameStart, typeNameEnd - typeNameStart);
-                //var endOfTypeName = parameterTypeName.IndexOf(',');
-                //var parameterType = base.BindToType(parameterTypeName.Substring(endOfTypeName + 2), parameterTypeName.Substring(0, endOfTypeName));
-
-                // resolution of the type thru the assembly of the base type fails
-                //Assembly assembly1 = Assembly.LoadWithPartialName(assemblyName);
-                //Type type = assembly1.GetType(typeName);
-
-                // resolution of the type thru the general type-resolution mechanism succeeds
-                return Type.GetType(string.Format("{0}, {1}", typeName, assemblyName));
-            }
-
-            return base.BindToType(assemblyName, typeName);
-        }
     }
 }
