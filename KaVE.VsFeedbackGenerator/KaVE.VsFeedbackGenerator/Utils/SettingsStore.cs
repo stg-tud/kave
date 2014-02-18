1c984a0b74ded2cf3229e9932e8a8270ccf93dbf
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2014-02-18 13:57:56 +0100

    * added R# settings store accessor

---
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/Utils/SettingsStore.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Application;
+using JetBrains.Application.DataContext;
+using JetBrains.Application.Settings;
+
+namespace KaVE.VsFeedbackGenerator.Utils
+{
+    [ShellComponent]
+    public class SettingsStore
+    {
+        private readonly ISettingsStore _settingsStore;
+        private readonly DataContexts _dataContexts;
+
+        public SettingsStore(ISettingsStore settingsStore, DataContexts dataContexts)
+        {
+            _settingsStore = settingsStore;
+            _dataContexts = dataContexts;
+        }
+
+        private IContextBoundSettingsStore ContextStore
+        {
+            get
+            {
+                var contextRange = ContextRange.Smart((lt, _) => _dataContexts.Empty);
+                return _settingsStore.BindToContextTransient(contextRange);
+            }
+        }
+
+        public TSettingsInstance GetSettings<TSettingsInstance>()
+        {
+            return ContextStore.GetKey<TSettingsInstance>(SettingsOptimization.DoMeSlowly);
+        }
+
+        public void SetSettings<TSettingsType>(TSettingsType settingsInstance) where TSettingsType : class
+        {
+            ContextStore.SetKey(settingsInstance, SettingsOptimization.DoMeSlowly);
+        }
+    }
+}
