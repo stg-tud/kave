05c7d61f27bb96d6fa84ba6989afe875fcf27de6
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2014-02-18 14:12:35 +0100

    * forgot project update...

---
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/UploadSettings.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Application.Settings;
+
+namespace KaVE.VsFeedbackGenerator.SessionManager
+{
+    [SettingsKey(typeof(System.Reflection.Missing), "KaVE Upload Settings")]
+    public class UploadSettings
+    {
+        [SettingsEntry("2012-02-13", "Upload Time")]
+        public string LastUploadTime { get; set; }
+    }
+}
