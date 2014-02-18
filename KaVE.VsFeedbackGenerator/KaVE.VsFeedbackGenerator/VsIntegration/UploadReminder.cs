0e73333f839917f09c1de3ddc96d7c907b4b99a1
Uli <uli.fahrer@googlemail.com>
2014-02-18 15:02:55 +0100

    * update settings & message boxes

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/VsIntegration/UploadReminder.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/VsIntegration/UploadReminder.cs
@@ -1,46 +1,73 @@
-﻿
-using System;
+﻿using System;
+using System.Globalization;
 using JetBrains.Application;
 using JetBrains.Threading;
-using JetBrains.UI.Tooltips;
 using JetBrains.Util;
 using KaVE.Utils;
+using KaVE.VsFeedbackGenerator.SessionManager;
 using KaVE.VsFeedbackGenerator.SessionManager.Presentation;
+using KaVE.VsFeedbackGenerator.Utils;
 
 namespace KaVE.VsFeedbackGenerator.VsIntegration
 {
     [ShellComponent]
     public class UploadReminder
     {
-        private System.Threading.Timer _weeklyTimer;
-        private ITooltipManager _tooltipManager;
+        private System.Threading.Timer _timer;
+        private readonly SettingsStore _settingsStore;
+        private readonly SessionManagerWindowRegistrar _sessionWindow;
         private const string SessionManagerActionId = "KaVE.VsFeedbackGenerator.SessionManager";
 
-        public UploadReminder(ITooltipManager tooltipManager)
+        public UploadReminder(SettingsStore settingsStore)
         {
-            _tooltipManager = tooltipManager;
-            _weeklyTimer = CreateWeeklyTimer(TimeSpan.FromMinutes(new Random().Next(0, 60)));
-        }
+            _settingsStore = settingsStore;
+            _sessionWindow = Shell.Instance.GetComponent<SessionManagerWindowRegistrar>();
 
-        private System.Threading.Timer CreateWeeklyTimer(TimeSpan minuteOffset)
-        {
-            return new System.Threading.Timer(ExecuteOnceAWeek, null, TimeSpan.FromMinutes(0.5), TimeSpan.Zero);
-        }
-
-        private void ExecuteOnceAWeek(Object obj)
-        {
-            var result = MessageBox.ShowYesNoCancel("Upload it or die");
-            if (result == true)
+            //Init today date for settings 
+            var settings = _settingsStore.GetSettings<UploadSettings>();
+            if (settings.LastUploadTime.IsEmpty())
             {
-                var window = Shell.Instance.GetComponent<SessionManagerWindowRegistrar>();
+                settings.LastUploadTime = DateTime.Today.ToString(CultureInfo.InvariantCulture);
+                settingsStore.SetSettings(settings);
+            }
 
-                Invoke.OnDispatcherAsync(
-                    new Action(
-                        () =>
-                            ReentrancyGuard.Current.Execute(
-                                SessionManagerActionId,
-                                new Action(() => window.ToolWindow.Show()))));
-             }
+            _timer = CreateTimer(TimeSpan.FromMinutes(0.5), TimeSpan.FromSeconds(new Random().Next(0, 60)));
         }
+
+        private System.Threading.Timer CreateTimer(TimeSpan intervall, TimeSpan minuteOffset)
+        {
+            return new System.Threading.Timer(ExecuteOnceAnHour, null, intervall.Add(minuteOffset), TimeSpan.Zero);
+        }
+
+        private void ExecuteOnceAnHour(Object obj)
+        {
+            var settings = _settingsStore.GetSettings<UploadSettings>();
+            var lastUpdate = DateTime.Parse(settings.LastUploadTime);
+            //Soft popup, because date is older than a week
+            if (lastUpdate > DateTime.Today.AddDays(7))
+            {
+                var result = MessageBox.ShowYesNoCancel("Upload it or die");
+                if (result == true)
+                {
+                    OpenSessionManager();
+                }
+            }
+            //Hard popup
+            else if (lastUpdate > DateTime.Today.AddMonths(1))
+            {
+                MessageBox.ShowExclamation("UPLOAD!!!!!!!!!!!!!!");
+                OpenSessionManager();
+            }
+
+            _timer = CreateTimer(TimeSpan.FromMinutes(0.5), TimeSpan.FromSeconds(new Random().Next(0, 60)));
+        }
+
+        private void OpenSessionManager()
+        {
+            Invoke.OnDispatcherAsync(
+                    () => ReentrancyGuard.Current.Execute(SessionManagerActionId,
+                        () => _sessionWindow.ToolWindow.Show()));
+        }
+
     }
 }
