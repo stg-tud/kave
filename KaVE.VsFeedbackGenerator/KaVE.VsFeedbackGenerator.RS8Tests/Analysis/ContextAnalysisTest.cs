cb49734af8ed44688ee831aa70397332b2c59fef
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2014-02-20 12:33:13 +0100

    (re)-implemented calculation of base path for tests

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator.RS8Tests/Analysis/ContextAnalysisTest.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator.RS8Tests/Analysis/ContextAnalysisTest.cs
@@ -1,9 +1,15 @@
-﻿using NUnit.Framework;
+﻿using JetBrains.Application;
+using NUnit.Framework;
 
 namespace KaVE.VsFeedbackGenerator.RS8Tests.Analysis
 {
     [TestFixture]
     class ContextAnalysisTest : KaVEBaseTest
     {
+        [Test]
+        public void ShouldRetrieveContext()
+        {
+            //Shell.Instance.GetComponent<ContextAnalysisComponent>();
+        }
     }
 }
--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator.RS8Tests/KaVEBaseTest.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator.RS8Tests/KaVEBaseTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.CodeCompletion;
@@ -7,18 +8,42 @@ using JetBrains.ReSharper.Feature.Services.Tests.CSharp.FeatureServices.CodeComp
 using JetBrains.ReSharper.Psi;
 using JetBrains.TextControl;
 using KaVE.Model.Events.CompletionEvent;
+using KaVE.Utils.Assertion;
 using KaVE.VsFeedbackGenerator.Utils;
 using NUnit.Framework;
 
 namespace KaVE.VsFeedbackGenerator.RS8Tests
 {
-    abstract class KaVEBaseTest : CodeCompletionTestBase
+    internal abstract class KaVEBaseTest : CodeCompletionTestBase
     {
         private readonly List<CodeCompletionType> _myCodeCompletionTypes;
 
         protected KaVEBaseTest()
         {
             _myCodeCompletionTypes = new List<CodeCompletionType> {CodeCompletionType.BasicCompletion};
+        }
+
+        protected override string RelativeTestDataPath
+        {
+            get
+            {
+                var defaultNamespace = typeof (KaVEBaseTest).Namespace;
+                var concreteNamespace = GetType().FullName;
+
+                Asserts.NotNull(defaultNamespace, "KaVEBaseTest somehow moved to global namespace?!");
+
+                string basePath;
+                if (concreteNamespace.StartsWith(defaultNamespace))
+                {
+                    basePath = concreteNamespace.Substring(defaultNamespace.Length + 1);
+                }
+                else
+                {
+                    basePath = concreteNamespace;
+                }
+
+                return Path.Combine(basePath.Split('.'));
+            }
         }
 
         protected override bool ExecuteAction
@@ -41,14 +66,22 @@ namespace KaVE.VsFeedbackGenerator.RS8Tests
             Assert.IsNotNull(psiSourceFile, "psiSourceFile == null");
             var projectFile = psiSourceFile.ToProjectFile();
             Assert.IsNotNull(projectFile, "projectFile == null");
-            var service = Solution.GetComponent<ILanguageManager>().TryGetService<CodeCompletionModifierProvider>(psiSourceFile.PrimaryPsiLanguage);
-            var parameters = service != null ? service.GetModifier(_myCodeCompletionTypes) : CodeCompletionModifierProvider.GetModifierBasic(_myCodeCompletionTypes.Take(_myCodeCompletionTypes.Count - 1).ToList(), _myCodeCompletionTypes[_myCodeCompletionTypes.Count - 1]);
+            var service =
+                Solution.GetComponent<ILanguageManager>()
+                    .TryGetService<CodeCompletionModifierProvider>(psiSourceFile.PrimaryPsiLanguage);
+            var parameters = service != null
+                ? service.GetModifier(_myCodeCompletionTypes)
+                : CodeCompletionModifierProvider.GetModifierBasic(
+                    _myCodeCompletionTypes.Take(_myCodeCompletionTypes.Count - 1).ToList(),
+                    _myCodeCompletionTypes[_myCodeCompletionTypes.Count - 1]);
 
             var codeCompletionResult = intellisenseManager.GetCompletionResult(parameters, textControl);
-            var best = ((SelectionStrategyWithPreferences)codeCompletionResult.SelectionStrategy).GetAllPreferredItems(codeCompletionResult.GetFilteredLookupItems()).ToHashSet();
+            var best =
+                ((SelectionStrategyWithPreferences) codeCompletionResult.SelectionStrategy).GetAllPreferredItems(
+                    codeCompletionResult.GetFilteredLookupItems()).ToHashSet();
             ResultProposalCollection = GetItemsFromResult(codeCompletionResult, best).ToProposalCollection();
         }
 
-        protected ProposalCollection ResultProposalCollection { get; private set; } 
+        protected ProposalCollection ResultProposalCollection { get; private set; }
     }
 }
