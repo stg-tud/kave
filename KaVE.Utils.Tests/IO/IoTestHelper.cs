05132f75af11252a9585c73a011e42732d44d8b5
Dennis Albrecht <dennis.albrecht@stud.tu-darmstadt.de>
2014-03-12 14:27:26 +0100

    * Helper for IO-Tests

---
+++ KaVE.Utils.Tests/IO/IoTestHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace KaVE.Utils.Tests.IO
+{
+    public static class IoTestHelper
+    {
+        private static readonly Random Random = new Random();
+
+        public static string GetTempDirectoryName()
+        {
+            var temp = Path.GetTempPath();
+            string dir;
+            do
+            {
+                dir = Path.Combine(temp, "dir-" + Random.Next());
+            } while (Directory.Exists(dir));
+            Directory.CreateDirectory(dir);
+            return dir;
+        }
+
+        public static string GetTempFileName(string dir)
+        {
+            string file;
+            do
+            {
+                file = Path.Combine(dir, "file-" + Random.Next());
+            } while (File.Exists(file));
+            File.Create(file);
+            return file;
+        }
+    }
+}
