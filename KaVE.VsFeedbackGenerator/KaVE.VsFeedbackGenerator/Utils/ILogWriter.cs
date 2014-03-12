64f1ad12364b80c8fc40211d97d981ceaadb8c9f
Dennis Albrecht <dennis.albrecht@stud.tu-darmstadt.de>
2014-03-12 15:01:00 +0100

    * ILogWriter extended to be Disposable

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/Utils/ILogWriter.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/Utils/ILogWriter.cs
@@ -1,8 +1,9 @@
+using System;
 using KaVE.JetBrains.Annotations;
 
 namespace KaVE.VsFeedbackGenerator.Utils
 {
-    public interface ILogWriter<in TMessage> {
+    public interface ILogWriter<in TMessage> : IDisposable {
         void Write([NotNull] TMessage message);
     }
 }
