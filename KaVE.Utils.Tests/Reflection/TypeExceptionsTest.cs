d9a2dcd61292ac0c36e2e7b8c107d679ffd5cd74
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2014-03-21 11:49:34 +0100

    * added helper to retrieve property names in a refactoring-safe way

---
+++ KaVE.Utils.Tests/Reflection/TypeExceptionsTest.cs
@@ -0,0 +1,36 @@
+﻿using KaVE.Utils.Reflection;
+using NUnit.Framework;
+
+namespace KaVE.Utils.Tests.Reflection
+{
+    [TestFixture]
+    internal class TypeExceptionsTest
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        private int MyTestProperty { get; set; }
+
+        [Test]
+        public void ShouldGetPropertyName()
+        {
+            var actual = TypeExtensions<TypeExceptionsTest>.GetPropertyName(o => o.MyTestProperty);
+
+            Assert.AreEqual("MyTestProperty", actual);
+        }
+
+        [Test(Description = "In some cases an implicit cast to object is introduced; this is tested explicitly here.")]
+        public void ShouldGetPropertyWithCast()
+        {
+            var actual = TypeExtensions<TypeExceptionsTest>.GetPropertyName(o => (object) o.MyTestProperty);
+
+            Assert.AreEqual("MyTestProperty", actual);
+        }
+
+        [Test,
+         ExpectedException(ExpectedMessage = "Invalid expression type: Expected ExpressionType.MemberAccess, Found Call"
+             )]
+        public void ShouldFailToGet()
+        {
+            TypeExtensions<TypeExceptionsTest>.GetPropertyName(o => o.Equals(null));
+        }
+    }
+}
---
+++ KaVE.Utils/Reflection/TypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace KaVE.Utils.Reflection
+{
+    public static class TypeExtensions<T>
+    {
+        public static string GetPropertyName<TProperty>(Expression<Func<T, TProperty>> expression)
+        {
+            if (expression.Body.NodeType == ExpressionType.MemberAccess)
+            {
+                return ((MemberExpression) expression.Body).Member.Name;
+            }
+
+            if ((expression.Body.NodeType == ExpressionType.Convert) && (expression.Body.Type == typeof (object)))
+            {
+                return ((MemberExpression) ((UnaryExpression) expression.Body).Operand).Member.Name;
+            }
+
+            throw new Exception(
+                string.Format(
+                    "Invalid expression type: Expected ExpressionType.MemberAccess, Found {0}",
+                    expression.Body.NodeType));
+        }
+    }
+}
