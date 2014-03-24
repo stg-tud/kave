674cb47a06bbf42f0a2a2394aa391644bb65d3b4
Uli <uli.fahrer@googlemail.com>
2014-03-24 22:41:23 +0100

    * first wizard draft

---
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/TrayNotification/UploadWizard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace KaVE.VsFeedbackGenerator.TrayNotification
+{
+    /// <summary>
+    /// Interaction logic for UploadWizard.xaml
+    /// </summary>
+    public partial class UploadWizard : Window
+    {
+        public UploadWizard()
+        {
+            InitializeComponent();
+        }
+    }
+}
