d80a3ea94ee5d238fe0471e360611065fa5a9e4b
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2014-04-29 10:47:33 +0200

    * NuSpec now builds plugin for 8.2 only * Changed some texts in the SessionManager

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator-build-nuget-package.bat
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator-build-nuget-package.bat
@@ -2,16 +2,19 @@
 
 :: this picks the first .csproj or .nuspec file in the working directory and builds a .nupkg from it
 :: to package a specific project or file, add the .csproj or .nupkg file name after the pack option
-:: -NoPackageAnalysis supresses warnings, caused by R# bundles not adhering to the nuget structure conventions
+:: -NoPackageAnalysis suppresses warnings, caused by R# bundles not adhering to the NuGet structure conventions
 
 set TIME1=%time::=%
-set TIME2=%TIME1:~0,-3%
+set TIME2=%TIME1: =0%
+set TIME3=%TIME2:~0,-3%
 ::echo %TIME2%
 
 set DATE1=%date:~-4,4%%date:~-7,2%%date:~-10,2%
 ::echo %DATE1%
 
-set TIMESTAMP=%DATE1%-%TIME2%
-::echo %TIMESTAMP%
+set TIMESTAMP=%DATE1%-%TIME3%
+echo Building KaVE Feedback Generator @ %TIMESTAMP%...
 
-..\.nuget\nuget pack -Verbosity detailed -NoPackageAnalysis -Version 1.0.0-v%TIMESTAMP%
+..\.nuget\NuGet.exe pack -Verbosity detailed -NoPackageAnalysis -Version 1.0.0-v%TIMESTAMP%
+
+pause
