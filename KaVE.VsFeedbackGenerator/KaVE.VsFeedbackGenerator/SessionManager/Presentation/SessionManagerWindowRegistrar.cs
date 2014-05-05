f77f0cd989abf7677f1804c4e2d3e140b44b8fa3
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2014-05-05 10:00:51 +0200

    * added comment that explaines unused fields

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/Presentation/SessionManagerWindowRegistrar.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/Presentation/SessionManagerWindowRegistrar.cs
@@ -2,19 +2,27 @@
 using JetBrains.DataFlow;
 using JetBrains.UI.CrossFramework;
 using JetBrains.UI.ToolWindowManagement;
+using KaVE.JetBrains.Annotations;
 
 namespace KaVE.VsFeedbackGenerator.SessionManager.Presentation
 {
     [ShellComponent]
     public class SessionManagerWindowRegistrar
     {
+        [UsedImplicitly]
+        private readonly Lifetime _lifetime;
+        [UsedImplicitly]
+        private readonly ToolWindowClass _toolWindowClass;
+
         public SessionManagerWindowRegistrar(Lifetime lifetime,
             ToolWindowManager toolWindowManager,
             SessionManagerWindowDescriptor descriptor,
             FeedbackViewModel feedbackViewModel)
         {
-            var toolWindowClass = toolWindowManager.Classes[descriptor];
-            toolWindowClass.RegisterEmptyContent(
+            // objects are kept in fields to prevent garbage collection
+            _lifetime = lifetime;
+            _toolWindowClass = toolWindowManager.Classes[descriptor];
+            _toolWindowClass.RegisterEmptyContent(
                 lifetime,
                 lt =>
                 {
