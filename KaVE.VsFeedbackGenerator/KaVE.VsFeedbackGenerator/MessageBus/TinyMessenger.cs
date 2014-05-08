791711a34a1e816a6746ab93fae5c327f31fdbaa
Dennis Albrecht <dennis.albrecht@stud.tu-darmstadt.de>
2014-05-08 19:46:42 +0200

    * undone license-change

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/MessageBus/TinyMessenger.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/MessageBus/TinyMessenger.cs
@@ -1,18 +1,16 @@
-﻿/*
- * Copyright 2014 Technische Universität Darmstadt
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+﻿//===============================================================================
+// TinyIoC - TinyMessenger
+//
+// A simple messenger/event aggregator.
+//
+// http://hg.grumpydev.com/tinyioc
+//===============================================================================
+// Copyright © Steven Robbins.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
 
 using System;
 using System.Collections.Generic;
