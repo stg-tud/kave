b7544dca8e44304b529dc5ec0026d16f1a21f3cf
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2014-05-08 13:28:09 +0200

    * switched upload settings to DateTime

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/UploadSettings.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/UploadSettings.cs
@@ -3,48 +3,24 @@ using JetBrains.Application.Settings;
 
 namespace KaVE.VsFeedbackGenerator.SessionManager
 {
-    // ReSharper disable CSharpWarnings::CS0612
-    [SettingsKey(typeof(System.Reflection.Missing), "KaVE Upload Settings")]
+    [SettingsKey(typeof(FeedbackSettings), "KaVE Feeback-Upload Settings")]
     public class UploadSettings
     {
-        // TODO can R# store DateTimes to save the manual convertion
-        [SettingsEntry(0, "Notification Time"), Obsolete]
-        public long LastNotificationTicks { get; set; }
+        private const string DateTimeMinValue = "01/01/0001 00:00:00";
 
-        [SettingsEntry(0, "Upload Time"), Obsolete]
-        public long LastUploadTicks { get; set; }
-
-        [SettingsEntry(false, "Do Not Show Again Flag")]
+        [SettingsEntry(false, "Wheather or not to show a confirmation dialog when closing the upload-reminder popup.")]
         public bool DoNotShowNotificationCloseDialogAgain { get; set; }
 
-        public DateTime LastNotificationDate
-        {
-            get { return Parse(LastNotificationTicks); }
+        [SettingsEntry(DateTimeMinValue, "Timestamp of the last time the upload-reminder popup was shown to the user.")]
+        public DateTime LastNotificationDate { get; set; }
 
-            set { LastNotificationTicks = Serialize(value); }
-        }
-
-        public DateTime LastUploadDate
-        {
-            get { return Parse(LastUploadTicks); }
-
-            set { LastUploadTicks = Serialize(value); }
-        }
-
-        private static DateTime Parse(long dateTimeSerialization)
-        {
-            return new DateTime(dateTimeSerialization);
-        }
-
-        private static long Serialize(DateTime dateTime)
-        {
-            return dateTime.Ticks;
-        }
+        [SettingsEntry(DateTimeMinValue, "Timestamp of the last time an export was done.")]
+        public DateTime LastUploadDate { get; set; }
 
         public bool IsInitialized()
         {
-            var hasUninitializedField = LastNotificationTicks == 0 ||
-                                        LastUploadTicks == 0;
+            var hasUninitializedField = LastNotificationDate == DateTime.MinValue ||
+                                        LastUploadDate == DateTime.MinValue;
             return !hasUninitializedField;
         }
 
@@ -56,5 +32,4 @@ namespace KaVE.VsFeedbackGenerator.SessionManager
             LastNotificationDate = now;
         }
     }
-    // ReSharper restore CSharpWarnings::CS0612
 }
