03f144bd19d8344620fce2bfefb728a3fe96f360
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2014-07-24 16:30:29 +0200

    * made sortable list view preserve sorting on item-source change

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/Presentation/SortableListView.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/Presentation/SortableListView.cs
@@ -18,6 +18,8 @@
  *    - Sven Amann
  */
 
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
@@ -25,13 +27,15 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Markup;
+using JetBrains.Util;
+using KaVE.Utils.Reflection;
 
 namespace KaVE.VsFeedbackGenerator.SessionManager.Presentation
 {
     public class SortableListView : ListView
     {
-        private GridViewColumnHeader _lastClickedHeader;
-        private ListSortDirection _lastDirection = ListSortDirection.Ascending;
+        private static readonly string ItemsSourcePropertyName =
+            TypeExtensions<ListView>.GetPropertyName(lv => lv.ItemsSource);
 
         private const string ArrowUpPathData = "M 5,10 L 15,10 L 10,5 L 5,10";
         private const string ArrowDownPathData = "M 5,5 L 10,10 L 15,5 L 5,5";
@@ -50,9 +54,30 @@ namespace KaVE.VsFeedbackGenerator.SessionManager.Presentation
         private static readonly DataTemplate ArrowDownHeaderTemplate =
             (DataTemplate) XamlReader.Parse(string.Format(ArrowHeaderDataTemplateRaw, ArrowDownPathData));
 
+        private GridViewColumnHeader _lastClickedHeader;
+        private ListSortDirection _lastDirection = ListSortDirection.Ascending;
+        private IList<SortDescription> _currentSortDescriptions;
+
         public SortableListView()
         {
             AddHandler(ButtonBase.ClickEvent, new RoutedEventHandler(OnGridViewColumnHeaderClicked));
+            TypeDescriptor.GetProperties(this)[ItemsSourcePropertyName].AddValueChanged(
+                this,
+                RestoreSortDescriptionsOnItemSourceChange);
+        }
+
+        private ICollectionView DataView
+        {
+            get { return CollectionViewSource.GetDefaultView(ItemsSource ?? Items); }
+        }
+
+        private void RestoreSortDescriptionsOnItemSourceChange(object sender, EventArgs eventArgs)
+        {
+            if (_currentSortDescriptions == null)
+            {
+                return;
+            }
+            SetSortDescription(_currentSortDescriptions);
         }
 
         private void OnGridViewColumnHeaderClicked(object sender, RoutedEventArgs e)
@@ -99,16 +124,25 @@ namespace KaVE.VsFeedbackGenerator.SessionManager.Presentation
 
         private static string GetBindingName(GridViewColumn column)
         {
-            return ((Binding)column.DisplayMemberBinding).Path.Path;
+            return ((Binding) column.DisplayMemberBinding).Path.Path;
         }
 
         private void SortBy(string primaryCriteria, string secondaryCriteria, ListSortDirection direction)
         {
-            var dataView = CollectionViewSource.GetDefaultView(ItemsSource ?? Items);
-            dataView.SortDescriptions.Clear();
-            dataView.SortDescriptions.Add(new SortDescription(primaryCriteria, direction));
-            dataView.SortDescriptions.Add(new SortDescription(secondaryCriteria, direction));
-            dataView.Refresh();
+            _currentSortDescriptions = new[]
+            {
+                new SortDescription(primaryCriteria, direction),
+                new SortDescription(secondaryCriteria, direction)
+            };
+
+            SetSortDescription(_currentSortDescriptions);
+            DataView.Refresh();
+        }
+
+        private void SetSortDescription(IEnumerable<SortDescription> sortDescriptions)
+        {
+            DataView.SortDescriptions.Clear();
+            DataView.SortDescriptions.AddRange(sortDescriptions);
         }
 
         private void ShowSortArrow(GridViewColumnHeader clickedHeader, ListSortDirection direction)
