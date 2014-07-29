8e6afc279fdc077ef384e3d5fff96f10339d510f
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2014-07-29 17:14:02 +0200

    validation of uploaded files

--- feedback-server/src/main/java/kave/UploadCleanser.java
+++ feedback-server/src/main/java/kave/UploadCleanser.java
@@ -18,10 +18,77 @@
  */
 package kave;
 
+import java.io.Closeable;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class UploadCleanser {
-    public File purify(File in) {
-        return in;
+
+    private UniqueFileCreator ufc;
+
+    public UploadCleanser(UniqueFileCreator ufc) {
+        this.ufc = ufc;
+    }
+
+    public File purify(File in) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        File out = ufc.createNextUniqueFile();
+        int num = 0;
+
+        ZipFile zfin = null;
+        ZipOutputStream zfout = null;
+
+        try {
+            zfin = new ZipFile(in);
+            zfout = new ZipOutputStream(new FileOutputStream(out));
+
+            Enumeration<? extends ZipEntry> entries = zfin.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                String nextFileName = (num++) + ".json";
+
+                InputStream zein = null;
+                try {
+                    zein = zfin.getInputStream(entry);
+                    @SuppressWarnings("unchecked")
+                    Map<String, Object> content = mapper.readValue(zein, Map.class);
+
+                    zfout.putNextEntry(new ZipEntry(nextFileName));
+                    mapper.writeValue(zfout, content);
+                    zfout.closeEntry();
+                } catch (JsonParseException jpe) {
+                    // just ignore file
+                } finally {
+                    IOUtils.closeQuietly(zein);
+                }
+
+            }
+        } finally {
+            closeQuietly(zfin);
+            zfout.close();
+        }
+
+        return out;
+    }
+
+    private static void closeQuietly(Closeable c) {
+        try {
+            if (c != null) {
+                c.close();
+            }
+        } catch (Exception e) {
+            // ignore
+        }
     }
 }
--- feedback-server/src/test/java/kave/UploadCleanserTest.java
+++ feedback-server/src/test/java/kave/UploadCleanserTest.java
@@ -19,17 +19,254 @@
 package kave;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.io.Closeable;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.google.common.collect.Maps;
 
 public class UploadCleanserTest {
+
+    @Rule
+    public TemporaryFolder root = new TemporaryFolder();
+    private FileBuilder fileBuilder;
+    private File FILE_IN;
+    private File FILE_OUT_EXPECTED;
+    private File actualOut;
+
+    private UniqueFileCreator ufc;
+    private UploadCleanser sut;
+
+    @Before
+    public void setup() throws IOException {
+        FILE_IN = new File(root.getRoot(), "in.zip");
+        FILE_OUT_EXPECTED = new File(root.getRoot(), "out.zip");
+
+        ufc = mock(UniqueFileCreator.class);
+        when(ufc.createNextUniqueFile()).then(new Answer<File>() {
+            @Override
+            public File answer(InvocationOnMock invocation) throws Throwable {
+                FILE_OUT_EXPECTED.createNewFile();
+                return FILE_OUT_EXPECTED;
+            }
+        });
+        sut = new UploadCleanser(ufc);
+    }
+
     @Test
-    public void doesReturnTheInput() {
-        UploadCleanser sut = new UploadCleanser();
-        File in = new File("QWERTY");
-        File out = sut.purify(in);
-        assertEquals(in, out);
+    public void fileNameIsRequestedFromUFC() throws IOException {
+        givenAFile().create();
+        purify();
+        verify(ufc).createNextUniqueFile();
+        assertEquals(FILE_OUT_EXPECTED, actualOut);
+    }
+
+    @Test
+    public void binaryContent() {
+        givenAFile().with("a.bin", new byte[] { 127, 13, 45, 114, 3, 72, 96 });
+
+        purify();
+
+        assertNumberOfFiles(0);
+    }
+
+    @Test
+    public void plainTextContent() {
+        givenAFile().with("b.txt", "some plain text");
+
+        purify();
+
+        assertNumberOfFiles(0);
+    }
+
+    @Test
+    public void jsonContent() {
+        givenAFile().with("0.json", json(123));
+
+        purify();
+
+        assertNumberOfFiles(1);
+        assertContents("0.json", json(123));
+    }
+
+    @Test
+    public void prettyPrintedJsonContentIsFlattened() {
+        givenAFile().with("0.json", jsonFancy(123));
+
+        purify();
+
+        assertNumberOfFiles(1);
+        assertContents("0.json", jsonFancyFlattened(123));
+    }
+
+    @Test
+    public void parseableJsonFilesAreRenamed() {
+        givenAFile().with("1.txt", json(0));
+
+        purify();
+
+        assertNumberOfFiles(1);
+        assertContents("0.json", json(0));
+    }
+
+    @Test
+    public void existingnumberingIsIgnored() {
+        givenAFile().with("1.json", json(0)).with("0.json", json(1));
+
+        purify();
+
+        assertNumberOfFiles(2);
+        assertContents("0.json", json(0));
+        assertContents("1.json", json(1));
+    }
+
+    @Test
+    public void subfoldersAreInlined() {
+        givenAFile().with("0.json", json(0)).with("sub/1.json", json(1)).with("2.json", json(2))
+                .with("sub/3.json", json(3));
+
+        purify();
+
+        assertNumberOfFiles(4);
+        assertContents("0.json", json(0));
+        assertContents("1.json", json(1));
+        assertContents("2.json", json(2));
+        assertContents("3.json", json(3));
+    }
+
+    private static String json(int num) {
+        return "{\"num\":" + num + "}";
+    }
+
+    private static String jsonFancy(int num) {
+        return "{\n\t\"num\":" + num + "\n}";
+    }
+
+    private static String jsonFancyFlattened(int num) {
+        return "{\"num\":" + num + "}";
+    }
+
+    private void purify() {
+        try {
+            fileBuilder.create();
+            actualOut = sut.purify(FILE_IN);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void assertNumberOfFiles(int expectedNumOfFiles) {
+        Set<String> actualNames = readZipFile(actualOut).keySet();
+        assertEquals(expectedNumOfFiles, actualNames.size());
+        for (int i = 0; i < expectedNumOfFiles; i++) {
+            String expectedName = i + ".json";
+            assertTrue(actualNames.contains(expectedName));
+        }
+    }
+
+    private void assertContents(String fileName, String expectedContent) {
+        String actualContent = readZipFile(actualOut).get(fileName);
+        assertNotNull(actualContent);
+        assertEquals(expectedContent, actualContent);
+    }
+
+    private Map<String, String> readZipFile(File f) {
+        Map<String, String> fileContent = Maps.newLinkedHashMap();
+        ZipFile zipFile = null;
+        InputStream entryStream = null;
+        try {
+            zipFile = new java.util.zip.ZipFile(actualOut);
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry ze = entries.nextElement();
+                String fileName = ze.getName();
+                entryStream = zipFile.getInputStream(ze);
+                String content = IOUtils.toString(entryStream);
+                entryStream.close();
+                fileContent.put(fileName, content);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeQuietly(zipFile);
+        }
+        return fileContent;
+    }
+
+    private static void closeQuietly(Closeable c) {
+        try {
+            if (c != null) {
+                c.close();
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
+    public FileBuilder givenAFile() {
+        fileBuilder = new FileBuilder(FILE_IN);
+        return fileBuilder;
+    }
+
+    public static class FileBuilder {
+
+        private File file;
+        Map<String, String> contents = Maps.newLinkedHashMap();
+
+        public FileBuilder(File file) {
+            this.file = file;
+        }
+
+        public FileBuilder with(String name, String content) {
+            contents.put(name, content);
+            return this;
+        }
+
+        public FileBuilder with(String name, byte[] content) {
+            contents.put(name, new String(content, Charset.forName("UTF-8")));
+            return this;
+        }
+
+        public File create() {
+            try {
+                final ZipOutputStream out = new ZipOutputStream(new FileOutputStream(file));
+
+                for (Entry<String, String> entry : contents.entrySet()) {
+                    out.putNextEntry(new ZipEntry(entry.getKey()));
+                    byte[] byteContent = entry.getValue().getBytes(Charset.forName("UTF-8"));
+                    out.write(byteContent, 0, byteContent.length);
+                    out.closeEntry();
+                }
+                out.close();
+
+                return file;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
