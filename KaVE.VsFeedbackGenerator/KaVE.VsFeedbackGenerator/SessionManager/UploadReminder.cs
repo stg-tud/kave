e69d8f5d83d20493638f243c39a68fd670d04089
Uli <uli.fahrer@googlemail.com>
2014-07-30 13:15:42 +0200

    * fixed failing tests

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/UploadReminder.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/SessionManager/UploadReminder.cs
@@ -48,8 +48,6 @@ namespace KaVE.VsFeedbackGenerator.SessionManager
 
             EnsureUploadSettingsInitialized();
             RegisterCallback();
-
-            _taskbarIcon.ShowHardBalloonPopup();
         }
 
         private void EnsureUploadSettingsInitialized()
