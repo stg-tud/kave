cb41f913f78a4ae87ea5a0a563cf9fa7365fda19
Uli <uli.fahrer@googlemail.com>
2014-07-31 16:45:16 +0200

    * hardpopup confirmation dialog removed

--- KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/TrayNotification/HardBalloonPopup.xaml.cs
+++ KaVE.VsFeedbackGenerator/KaVE.VsFeedbackGenerator/TrayNotification/HardBalloonPopup.xaml.cs
@@ -19,8 +19,6 @@
 
 using System.Windows;
 using System.Windows.Input;
-using KaVE.VsFeedbackGenerator.SessionManager;
-using KaVE.VsFeedbackGenerator.Utils;
 
 namespace KaVE.VsFeedbackGenerator.TrayNotification
 {
@@ -42,43 +40,6 @@ namespace KaVE.VsFeedbackGenerator.TrayNotification
         private void ImgClose_MouseDown(object sender, MouseButtonEventArgs e)
         {
             ClosePopup();
-        }
-
-        private new void ClosePopup()
-        {
-            var settingsStore = Registry.GetComponent<ISettingsStore>();
-            var settings = settingsStore.GetSettings<UploadSettings>();
-            if (settings.DoNotShowNotificationCloseDialogAgain)
-            {
-                base.ClosePopup();
-            }
-            else
-            {
-                ClosePopupWithConfirmation();
-            }
-        }
-
-        private void ClosePopupWithConfirmation()
-        {
-            var dialog = new PopupAbortConfirmationDialog();
-            dialog.ShowDialog();
-
-            if (dialog.Confirmed)
-            {
-                SaveDontShowAgainChoice(dialog.DontShowAgain);
-                base.ClosePopup();
-            }
-        }
-
-        private static void SaveDontShowAgainChoice(bool dontShowAgain)
-        {
-            if (dontShowAgain)
-            {
-                var settingsStore = Registry.GetComponent<ISettingsStore>();
-                var settings = settingsStore.GetSettings<UploadSettings>();
-                settings.DoNotShowNotificationCloseDialogAgain = true;
-                settingsStore.SetSettings(settings);
-            }
         }
     }
 }
