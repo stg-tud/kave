6b2dabde2091d294f86e04851c406e6c5694ed08
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-03-24 16:31:16 +0100

    Set up analysis infrastructure and test environment.

---
+++ feedback-generator/KaVE.SolutionAnalysis/KaVE.SolutionAnalysis/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using JetBrains.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KaVE.SolutionAnalysis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KaVE")]
+[assembly: AssemblyProduct("KaVE.SolutionAnalysis")]
+[assembly: AssemblyCopyright("Copyright © KaVE, 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// The following information is displayed by ReSharper in the Plugins dialog
+[assembly: PluginTitle("KaVE Solution Analysis")]
+[assembly: PluginDescription("")]
+[assembly: PluginVendor("KaVE")]
+
+[assembly: InternalsVisibleTo("KaVE.SolutionAnalysis.Tests")]
---
+++ feedback-generator/KaVE.SolutionAnalysis/KaVE.SolutionAnalysis/SolutionAnalysis.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Sven Amann
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Application;
+using JetBrains.ProjectModel;
+using JetBrains.Threading;
+using JetBrains.Util;
+
+namespace KaVE.SolutionAnalysis
+{
+    [SolutionComponent]
+    public class SolutionAnalysis
+    {
+        public static IList<String> AnalysedProjects = new List<String>();
+
+        public SolutionAnalysis(ISolution solution)
+        {
+            // Do everything in Analyse(), because in the test environment the solution is not ready yet
+            // and thus the test calls StartFromTestContext() when this is the case.
+            Analyse(solution);
+        }
+
+        private static void Analyse(ISolution solution)
+        {
+            var projects = solution.GetAllProjects();
+            AnalysedProjects.AddRange(projects.Select(project => project.Name));
+        }
+
+        internal static void StartFromTestContext(ISolution solution)
+        {
+            ReentrancyGuard.Current.Execute(
+                "solution analysis",
+                () =>
+                    ReadLockCookie.Execute(
+                        () => Analyse(solution)));
+        }
+    }
+}
