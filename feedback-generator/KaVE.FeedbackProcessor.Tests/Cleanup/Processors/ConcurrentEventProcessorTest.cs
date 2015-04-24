3d89def746fae8a76b37a6f3302e199621db8576
markuszm <markus_zimmermann-ffm@t-online.de>
2015-04-24 19:05:12 +0200

    Added detection of concurrent events

---
+++ feedback-generator/KaVE.FeedbackProcessor.Tests/Cleanup/Processors/ConcurrentEventProcessorTest.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Mattis Manfred Kämmerer
+ *    - Markus Zimmermann
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KaVE.Commons.Model.Events;
+using KaVE.Commons.TestUtils.Model.Events;
+using KaVE.Commons.Utils.Collections;
+using KaVE.FeedbackProcessor.Cleanup.Processors;
+using KaVE.FeedbackProcessor.Model;
+using NUnit.Framework;
+
+namespace KaVE.FeedbackProcessor.Tests.Cleanup.Processors
+{
+    [TestFixture]
+    internal class ConcurrentEventProcessorTest
+    {
+        private ConcurrentEventProcessor _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new ConcurrentEventProcessor();
+        }
+
+        [Test]
+        public void GeneratesConcurrentEventForConcurrentEvents()
+        {
+            var concurrentEventList = GenerateEvents(ConcurrentEventProcessor.EventTimeDifference.Ticks);
+
+            var expectedEventSet = Sets.NewHashSet(
+                concurrentEventList.Last(),
+                new ConcurrentEvent
+                {
+                    ConcurrentEventList = concurrentEventList.GetRange(0,concurrentEventList.Count-1),
+                    Duration = concurrentEventList.Last().TerminatedAt - concurrentEventList.First().TriggeredAt,
+                    TriggeredAt = concurrentEventList.First().TriggeredAt
+                }
+            );
+
+            IKaVESet<IDEEvent> resultEventSet = new KaVEHashSet<IDEEvent>();
+            concurrentEventList.ForEach(ideEvent => resultEventSet = _uut.Process(ideEvent));
+
+            CollectionAssert.AreEquivalent(expectedEventSet,resultEventSet);
+        }
+
+        [Test]
+        public void IgnoresNotConcurrentEvents()
+        {
+            var eventList = GenerateEvents(ConcurrentEventProcessor.EventTimeDifference.Ticks + 1);
+
+            eventList.ForEach(ideEvent => CollectionAssert.AreEquivalent(Sets.NewHashSet(ideEvent),_uut.Process(ideEvent)));
+        }
+
+        private static List<IDEEvent> GenerateEvents(long eventTimeDifferenceInTicks)
+        {
+            var eventList = IDEEventTestFactory.SomeEvents(2);
+            var lastEventTime = new DateTime(1984, 10, 15, 13, 0, 2);
+            eventList.ForEach(
+                ideEvent =>
+                {
+                    lastEventTime = new DateTime(lastEventTime.Ticks + eventTimeDifferenceInTicks);
+                    ideEvent.TriggeredAt = lastEventTime;
+                });
+
+            var lateEvent = IDEEventTestFactory.SomeEvent();
+            lateEvent.TriggeredAt = new DateTime(lastEventTime.Ticks + eventTimeDifferenceInTicks + 1);
+            eventList.Add(lateEvent);
+
+            return eventList;
+        }
+    }
+}
---
+++ feedback-generator/KaVE.FeedbackProcessor/Cleanup/Processors/ConcurrentEventProcessor.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Mattis Manfred Kämmerer
+ *    - Markus Zimmermann
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KaVE.Commons.Model.Events;
+using KaVE.Commons.Utils.Assertion;
+using KaVE.Commons.Utils.Collections;
+using KaVE.FeedbackProcessor.Model;
+
+namespace KaVE.FeedbackProcessor.Cleanup.Processors
+{
+    internal class ConcurrentEventProcessor : BaseProcessor
+    {
+        public static TimeSpan EventTimeDifference = new TimeSpan(0, 0, 0, 0, 1);
+
+        private readonly IList<IDEEvent> _eventCache;
+
+        public ConcurrentEventProcessor()
+        {
+            _eventCache = new List<IDEEvent>();
+
+            RegisterFor<IDEEvent>(ProcessIDEEvent);
+        }
+
+        private IKaVESet<IDEEvent> ProcessIDEEvent(IDEEvent @event)
+        {
+            if (_eventCache.Count == 0)
+            {
+                _eventCache.Add(@event);
+            }
+            else
+            {
+                var lastEventTime = _eventCache.Last().TriggeredAt;
+                var currentEventTime = @event.TriggeredAt;
+
+                AssertValidEventTimes(lastEventTime, currentEventTime);
+
+                if (CompareEventTimeDifference(currentEventTime, lastEventTime))
+                {
+                    _eventCache.Add(@event);
+                }
+                else
+                {
+                    var concurrentEvent = GenerateConcurrentEvent();
+                    _eventCache.Clear();
+                    if (MoreThanOneEventConcurredIn(concurrentEvent))
+                    {
+                        return AnswerInsert(concurrentEvent);
+                    }
+                }
+            }
+
+            return AnswerKeep();
+        }
+
+        private static bool MoreThanOneEventConcurredIn(ConcurrentEvent concurrentEvent)
+        {
+            return concurrentEvent.ConcurrentEventList.Count > 1;
+        }
+
+        private static void AssertValidEventTimes(DateTime? lastEventTime, DateTime? currentEventTime)
+        {
+            if (!lastEventTime.HasValue || !currentEventTime.HasValue)
+            {
+                Asserts.Fail("Events should have a DateTime value in TriggeredAt");
+            }
+        }
+
+        private static bool CompareEventTimeDifference(DateTime? currentEventTime, DateTime? lastEventTime)
+        {
+            var timeDifference = Math.Abs(currentEventTime.Value.Ticks - lastEventTime.Value.Ticks);
+            return timeDifference <= EventTimeDifference.Ticks;
+        }
+
+        private ConcurrentEvent GenerateConcurrentEvent()
+        {
+            return new ConcurrentEvent
+            {
+                ConcurrentEventList = new List<IDEEvent>(_eventCache),
+                IDESessionUUID = _eventCache.First().IDESessionUUID,
+                Id = _eventCache.First().Id,
+                TriggeredAt = _eventCache.First().TriggeredAt,
+                TriggeredBy = _eventCache.First().TriggeredBy,
+                TerminatedAt = _eventCache.Last().TerminatedAt,
+                Duration = _eventCache.Last().TerminatedAt - _eventCache.First().TriggeredAt,
+                KaVEVersion = _eventCache.First().KaVEVersion
+            };
+        }
+    }
+}
