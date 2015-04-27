f009e65639b2ef78e95ba3fc2ed954ca3a9a4576
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-04-27 15:09:39 +0200

    Computing events/day/developer statistic.

---
+++ feedback-generator/KaVE.FeedbackProcessor.Tests/Statistics/EventsPerDeveloperDayStatisticCalculatorTest.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Sven Amann
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KaVE.Commons.TestUtils.Model.Events;
+using KaVE.FeedbackProcessor.Model;
+using KaVE.FeedbackProcessor.Statistics;
+using MongoDB.Bson;
+using NUnit.Framework;
+
+namespace KaVE.FeedbackProcessor.Tests.Statistics
+{
+    [TestFixture]
+    internal class EventsPerDeveloperDayStatisticCalculatorTest
+    {
+        [Test]
+        public void CountsEventsOnSameDay()
+        {
+            var uut = new EventsPerDeveloperDayStatisticCalculator();
+
+            var someDeveloper = SomeDeveloper();
+            uut.NewDeveloper(someDeveloper);
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 28, 10, 52, 13)});
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 28, 16, 06, 00)});
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 28, 23, 59, 59)});
+
+            var statistic = uut.GetStatistic();
+            var developerDays = statistic[someDeveloper];
+            var developerDay = developerDays.First();
+            Assert.AreEqual(3, developerDay.NumberOfEvents);
+        }
+
+        [Test]
+        public void CountsEventsPerDay()
+        {
+            var uut = new EventsPerDeveloperDayStatisticCalculator();
+
+            var someDeveloper = SomeDeveloper();
+            uut.NewDeveloper(someDeveloper);
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 28, 10, 52, 13)});
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 29, 16, 06, 00)});
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 30, 23, 59, 59)});
+
+            var statistic = uut.GetStatistic();
+            var developerDays = statistic[someDeveloper];
+            Assert.AreEqual(3, developerDays.Count);
+        }
+
+        [Test]
+        public void CountsEventsPerDeveloper()
+        {
+            var uut = new EventsPerDeveloperDayStatisticCalculator();
+
+            var developer1 = SomeDeveloper();
+            uut.NewDeveloper(developer1);
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 28, 10, 52, 13)});
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 29, 16, 06, 00)});
+
+            var developer2 = SomeDeveloper();
+            uut.NewDeveloper(developer2);
+            uut.Process(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 30, 23, 59, 59)});
+
+            var statistic = uut.GetStatistic();
+            Assert.AreEqual(2, statistic.Count);
+            var developer1Days = statistic[developer1];
+            Assert.AreEqual(2, developer1Days.Count);
+            var developer2Days = statistic[developer2];
+            Assert.AreEqual(1, developer2Days.Count);
+        }
+
+        private static int _developerId = 1;
+        public static Developer SomeDeveloper()
+        {
+            var someDeveloper = new Developer{Id = new ObjectId(string.Format("{0:D24}", _developerId))};
+            _developerId++;
+            return someDeveloper;
+        }
+    }
+}
---
+++ feedback-generator/KaVE.FeedbackProcessor/Statistics/EventsPerDeveloperDayStatisticCalculator.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Sven Aman
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KaVE.Commons.Model.Events;
+using KaVE.Commons.Utils.Assertion;
+using KaVE.Commons.Utils.Collections;
+using KaVE.FeedbackProcessor.Cleanup;
+using KaVE.FeedbackProcessor.Model;
+
+namespace KaVE.FeedbackProcessor.Statistics
+{
+    internal class EventsPerDeveloperDayStatisticCalculator : IIDEEventProcessor
+    {
+        private readonly IDictionary<Developer, IDictionary<DateTime, DeveloperDay>> _developerDays = new Dictionary
+            <Developer, IDictionary<DateTime, DeveloperDay>>();
+
+        private IDictionary<DateTime, DeveloperDay> _currentDeveloperDays;
+
+        public void NewDeveloper(Developer developer)
+        {
+            _developerDays[developer] = new Dictionary<DateTime, DeveloperDay>();
+            _currentDeveloperDays = _developerDays[developer];
+        }
+
+        public IKaVESet<IDEEvent> Process(IDEEvent @event)
+        {
+            Asserts.That(@event.TriggeredAt.HasValue, "event without trigger time cannot be assigned to date");
+            var day = @event.TriggeredAt.Value.Date;
+            var developerDay = FindOrCreateDeveloperDay(day);
+            developerDay.NumberOfEvents++;
+            return null;
+        }
+
+        private DeveloperDay FindOrCreateDeveloperDay(DateTime day)
+        {
+            DeveloperDay developerDay;
+            if (!_currentDeveloperDays.TryGetValue(day, out developerDay))
+            {
+                developerDay = new DeveloperDay {Date = day};
+                _currentDeveloperDays.Add(day, developerDay);
+            }
+            return developerDay;
+        }
+
+        public IDictionary<Developer, IList<DeveloperDay>> GetStatistic()
+        {
+            return
+                _developerDays
+                    .ToDictionary
+                    <KeyValuePair<Developer, IDictionary<DateTime, DeveloperDay>>, Developer, IList<DeveloperDay>>(
+                        developerDays => developerDays.Key,
+                        developerDays => developerDays.Value.Values.ToList());
+        }
+
+        public class DeveloperDay
+        {
+            public int NumberOfEvents { get; set; }
+            public DateTime Date { get; set; }
+        }
+    }
+}
