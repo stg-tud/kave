5f491ae465f30577c2e7cdedccdef32eb5e1812a
M8is <M8is@live.de>
2015-05-01 15:55:49 +0200

    Added a Statistic Calculator that maps Concurrent Events to string sets

---
+++ feedback-generator/KaVE.FeedbackProcessor.Tests/Statistics/ConcurrentSetsCalculatorTest.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Mattis Manfred Kämmerer
+ *    - Markus Zimmermann
+ */
+
+using System.Collections.Generic;
+using KaVE.Commons.Model.Events;
+using KaVE.Commons.Model.Events.CompletionEvents;
+using KaVE.Commons.Model.Events.VisualStudio;
+using KaVE.Commons.TestUtils.Model.Events;
+using KaVE.Commons.Utils.Collections;
+using KaVE.FeedbackProcessor.Model;
+using KaVE.FeedbackProcessor.Statistics;
+using NUnit.Framework;
+
+namespace KaVE.FeedbackProcessor.Tests.Statistics
+{
+    [TestFixture]
+    internal class ConcurrentSetsCalculatorTest
+    {
+        private ConcurrentSetsCalculator _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new ConcurrentSetsCalculator();
+        }
+
+        private static readonly object[] MapsSingleEventCorrectlyTestCaseSource =
+        {
+            new object[] {new CommandEvent {CommandId = "Test"}, "Command -> Test"},
+            new object[] {new WindowEvent {Action = WindowEvent.WindowAction.Deactivate}, "Window -> Deactivate"},
+            new object[] {new DocumentEvent {Action = DocumentEvent.DocumentAction.Opened}, "Document -> Opened"},
+            new object[] {new BuildEvent {Action = "vsBuildActionBuild"}, "Build -> vsBuildActionBuild"},
+            new object[] {new EditEvent {NumberOfChanges = 1984}, "Edit -> 1984 Changes"},
+            new object[]
+            {
+                new DebuggerEvent {Reason = "dbgEventReasonStopDebugging"},
+                "Debugger -> dbgEventReasonStopDebugging"
+            },
+            new object[]
+            {
+                new IDEStateEvent {IDELifecyclePhase = IDEStateEvent.LifecyclePhase.Shutdown},
+                "IDEState -> Shutdown"
+            },
+            new object[]
+            {
+                new SolutionEvent {Action = SolutionEvent.SolutionAction.RenameProject},
+                "Solution -> RenameProject"
+            },
+            new object[]
+            {
+                new CompletionEvent {TerminatedState = TerminationState.Unknown},
+                "Completion -> Terminated as Unknown"
+            }
+        };
+
+        [Test, TestCaseSource("MapsSingleEventCorrectlyTestCaseSource")]
+        public void MapsSingleEventCorrectly(IDEEvent inputEvent, string expectedString)
+        {
+            var concurrentEvent = new ConcurrentEvent
+            {
+                ConcurrentEventList = new List<IDEEvent> {inputEvent}
+            };
+
+            _uut.OnEvent(concurrentEvent);
+
+            CollectionAssert.Contains(
+                _uut.Statistic,
+                new KeyValuePair<ISet<string>, int>(Sets.NewHashSet(expectedString), 1));
+        }
+
+        [Test]
+        public void CountsEqualSetsCorrectly()
+        {
+            var concurrentEvent = new ConcurrentEvent
+            {
+                ConcurrentEventList = new List<IDEEvent>
+                {
+                    new CommandEvent {CommandId = "Test"},
+                    new DocumentEvent {Action = DocumentEvent.DocumentAction.Saved}
+                }
+            };
+
+            _uut.OnEvent(concurrentEvent);
+            _uut.OnEvent(concurrentEvent);
+
+            Assert.AreEqual(2, _uut.Statistic[Sets.NewHashSet("Document -> Saved", "Command -> Test")]);
+        }
+
+        [Test]
+        public void ShouldIgnoreAllOtherEvents()
+        {
+            _uut.OnEvent(IDEEventTestFactory.SomeEvent());
+
+            CollectionAssert.IsEmpty(_uut.Statistic);
+        }
+
+        [Test]
+        public void ShouldDoNothingOnStreamStart()
+        {
+            _uut.OnStreamStarts(new Developer());
+
+            CollectionAssert.IsEmpty(_uut.Statistic);
+        }
+
+        [Test]
+        public void ShouldDoNothingOnStreamEnd()
+        {
+            _uut.OnStreamEnds();
+
+            CollectionAssert.IsEmpty(_uut.Statistic);
+        }
+    }
+}
---
+++ feedback-generator/KaVE.FeedbackProcessor/Statistics/ConcurrentSetsCalculator.cs
@@ -0,0 +1,147 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Mattis Manfred Kämmerer
+ *    - Markus Zimmermann
+ */
+
+using System.Collections.Generic;
+using KaVE.Commons.Model.Events;
+using KaVE.Commons.Model.Events.CompletionEvents;
+using KaVE.Commons.Model.Events.VisualStudio;
+using KaVE.Commons.Utils.Collections;
+using KaVE.FeedbackProcessor.Model;
+
+namespace KaVE.FeedbackProcessor.Statistics
+{
+    internal class ConcurrentSetsCalculator : IEventProcessor
+    {
+        private const string Separator = " -> ";
+
+        public readonly IDictionary<ISet<string>, int> Statistic =
+            new Dictionary<ISet<string>, int>();
+
+
+        public void OnEvent(IDEEvent @event)
+        {
+            var concurrentEvent = @event as ConcurrentEvent;
+            if (concurrentEvent == null)
+            {
+                return;
+            }
+
+            var resultSet = Sets.NewHashSet<string>();
+
+            foreach (var ideEvent in concurrentEvent.ConcurrentEventList)
+            {
+                if (ideEvent is CommandEvent)
+                {
+                    resultSet.Add(MapToString((CommandEvent) ideEvent));
+                }
+                if (ideEvent is WindowEvent)
+                {
+                    resultSet.Add(MapToString((WindowEvent) ideEvent));
+                }
+                if (ideEvent is DocumentEvent)
+                {
+                    resultSet.Add(MapToString((DocumentEvent) ideEvent));
+                }
+                if (ideEvent is BuildEvent)
+                {
+                    resultSet.Add(MapToString((BuildEvent) ideEvent));
+                }
+                if (ideEvent is EditEvent)
+                {
+                    resultSet.Add(MapToString((EditEvent) ideEvent));
+                }
+                if (ideEvent is DebuggerEvent)
+                {
+                    resultSet.Add(MapToString((DebuggerEvent) ideEvent));
+                }
+                if (ideEvent is IDEStateEvent)
+                {
+                    resultSet.Add(MapToString((IDEStateEvent) ideEvent));
+                }
+                if (ideEvent is SolutionEvent)
+                {
+                    resultSet.Add(MapToString((SolutionEvent) ideEvent));
+                }
+                if (ideEvent is CompletionEvent)
+                {
+                    resultSet.Add(MapToString((CompletionEvent) ideEvent));
+                }
+            }
+
+            if (Statistic.ContainsKey(resultSet))
+            {
+                Statistic[resultSet]++;
+            }
+            else
+            {
+                Statistic.Add(resultSet, 1);
+            }
+        }
+
+        private static string MapToString(CommandEvent commandEvent)
+        {
+            return string.Format("Command{0}{1}", Separator, commandEvent.CommandId);
+        }
+
+        private static string MapToString(WindowEvent windowEvent)
+        {
+            return string.Format("Window{0}{1}", Separator, windowEvent.Action);
+        }
+
+        private static string MapToString(DocumentEvent documentEvent)
+        {
+            return string.Format("Document{0}{1}", Separator, documentEvent.Action);
+        }
+
+        private static string MapToString(BuildEvent buildEvent)
+        {
+            return string.Format("Build{0}{1}", Separator, buildEvent.Action);
+        }
+
+        private static string MapToString(EditEvent editEvent)
+        {
+            return string.Format("Edit{0}{1}", Separator, editEvent.NumberOfChanges + " Changes");
+        }
+
+        private static string MapToString(DebuggerEvent debuggerEvent)
+        {
+            return string.Format("Debugger{0}{1}", Separator, debuggerEvent.Reason);
+        }
+
+        private static string MapToString(IDEStateEvent ideStateEvent)
+        {
+            return string.Format("IDEState{0}{1}", Separator, ideStateEvent.IDELifecyclePhase);
+        }
+
+        private static string MapToString(SolutionEvent solutionEvent)
+        {
+            return string.Format("Solution{0}{1}", Separator, solutionEvent.Action);
+        }
+
+        private static string MapToString(ICompletionEvent completionEvent)
+        {
+            return string.Format("Completion{0}{1}", Separator, "Terminated as " + completionEvent.TerminatedState);
+        }
+
+        public void OnStreamStarts(Developer value) {}
+
+        public void OnStreamEnds() {}
+    }
+}
