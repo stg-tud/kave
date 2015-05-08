08042c99f8c9df0958abba73348acd33db753a6e
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-05-08 10:23:14 +0200

    Naive repair of shutdown events.

---
+++ feedback-generator/KaVE.FeedbackProcessor.Tests/Cleanup/Processors/IDECloseEventFixingProcessorTest.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Sven Amann
+ */
+
+using KaVE.Commons.Model.Events.VisualStudio;
+using KaVE.Commons.TestUtils.Model.Events;
+using KaVE.FeedbackProcessor.Cleanup.Processors;
+using NUnit.Framework;
+
+namespace KaVE.FeedbackProcessor.Tests.Cleanup.Processors
+{
+    [TestFixture]
+    internal class IDECloseEventFixingProcessorTest
+    {
+        private static readonly IDEStateEvent StartupEvent = new IDEStateEvent
+        {
+            IDELifecyclePhase = IDEStateEvent.LifecyclePhase.Startup
+        };
+
+        private static readonly IDEStateEvent ShutdownEvent = new IDEStateEvent
+        {
+            IDELifecyclePhase = IDEStateEvent.LifecyclePhase.Shutdown
+        };
+
+        private IDECloseEventFixingProcessor _uut;
+
+        [SetUp]
+        public void CreateProcessor()
+        {
+            _uut = new IDECloseEventFixingProcessor();
+        }
+
+        [Test]
+        public void InsertsNothingBeforeFirstStartupEvent()
+        {
+            var actuals = _uut.Process(StartupEvent);
+
+            CollectionAssert.AreEquivalent(new[] {StartupEvent}, actuals);
+        }
+
+        [Test]
+        public void InsertsNothingIfStartupIsPreceededByShutdown()
+        {
+            _uut.Process(StartupEvent);
+            _uut.Process(ShutdownEvent);
+            var actuals = _uut.Process(StartupEvent);
+
+            CollectionAssert.AreEquivalent(new[] {StartupEvent}, actuals);
+        }
+
+        [Test]
+        public void InsertsShutdownBeforeStartupPreceededByEventOtherThanShutdown()
+        {
+            _uut.Process(StartupEvent);
+            _uut.Process(IDEEventTestFactory.SomeEvent());
+            var actuals = _uut.Process(StartupEvent);
+
+            CollectionAssert.AreEquivalent(new [] {StartupEvent, ShutdownEvent}, actuals);
+        }
+    }
+}
---
+++ feedback-generator/KaVE.FeedbackProcessor/Cleanup/Processors/IDECloseEventFixingProcessor.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Sven Amann
+ */
+
+using KaVE.Commons.Model.Events.VisualStudio;
+
+namespace KaVE.FeedbackProcessor.Cleanup.Processors
+{
+    internal class IDECloseEventFixingProcessor : BaseProcessor
+    {
+        private bool _ideIsRunning;
+
+        public IDECloseEventFixingProcessor()
+        {
+            RegisterFor<IDEStateEvent>(ProcessIDEStateEvent);
+        }
+
+        private void ProcessIDEStateEvent(IDEStateEvent @event)
+        {
+            switch (@event.IDELifecyclePhase)
+            {
+                case IDEStateEvent.LifecyclePhase.Startup:
+                    if (_ideIsRunning)
+                    {
+                        Insert(CreateMissingShutdownEvent());
+                    }
+                    _ideIsRunning = true;
+                    break;
+                case IDEStateEvent.LifecyclePhase.Shutdown:
+                    _ideIsRunning = false;
+                    break;
+            }
+        }
+
+        private static IDEStateEvent CreateMissingShutdownEvent()
+        {
+            return new IDEStateEvent {IDELifecyclePhase = IDEStateEvent.LifecyclePhase.Shutdown};
+        }
+    }
+}
