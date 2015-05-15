4c3774c65d38e6999daa9e30542f402de24f6d7c
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-05-15 13:02:45 +0200

    Fixed tests.

--- feedback-generator/KaVE.FeedbackProcessor.Tests/Statistics/EventsPerDeveloperDayStatisticCalculatorTest.cs
+++ feedback-generator/KaVE.FeedbackProcessor.Tests/Statistics/EventsPerDeveloperDayStatisticCalculatorTest.cs
@@ -64,51 +64,30 @@ namespace KaVE.FeedbackProcessor.Tests.Statistics
             _uut.OnEvent(new TestIDEEvent {TriggeredAt = new DateTime(2015, 4, 30, 23, 59, 59)});
             _uut.OnStreamEnds();
 
-            var expected = new[]
-            {
-                new EventsPerDeveloperDayStatisticCalculator.DeveloperDay(new DateTime(2015, 4, 28)),
-                new EventsPerDeveloperDayStatisticCalculator.DeveloperDay(new DateTime(2015, 4, 29)),
-                new EventsPerDeveloperDayStatisticCalculator.DeveloperDay(new DateTime(2015, 4, 30))
-            };
-            var actual = _uut.Statistic[someDeveloper];
-            CollectionAssert.AreEqual(expected, actual);
+            var actuals = _uut.Statistic[someDeveloper];
+            Assert.AreEqual(new DateTime(2015, 4, 28), actuals[0].Day);
+            Assert.AreEqual(new DateTime(2015, 4, 29), actuals[1].Day);
+            Assert.AreEqual(new DateTime(2015, 4, 30), actuals[2].Day);
         }
 
         [Test]
         public void CountsEventsPerDeveloper()
         {
             var developer1 = TestFactory.SomeDeveloper();
-            _uut.OnStreamStarts(developer1);
             var event1 = TestFactory.SomeEventFor(developer1);
+            var developer2 = TestFactory.SomeDeveloper();
+            var event2 = TestFactory.SomeEventFor(developer2);
+
+            _uut.OnStreamStarts(developer1);
             _uut.OnEvent(event1);
             _uut.OnStreamEnds();
-            var developer2 = TestFactory.SomeDeveloper();
             _uut.OnStreamStarts(developer2);
-            var event2 = TestFactory.SomeEventFor(developer2);
             _uut.OnEvent(event2);
             _uut.OnStreamEnds();
 
-            // ReSharper disable PossibleInvalidOperationException
-            var expected = new Dictionary<Developer, IList<EventsPerDeveloperDayStatisticCalculator.DeveloperDay>>
-            {
-                {
-                    developer1,
-                    new[]
-                    {
-                        new EventsPerDeveloperDayStatisticCalculator.DeveloperDay(event1.TriggeredAt.Value.Date)
-                    }
-                },
-                {
-                    developer2,
-                    new[]
-                    {
-                        new EventsPerDeveloperDayStatisticCalculator.DeveloperDay(event2.TriggeredAt.Value.Date)
-                    }
-                }
-            };
-            // ReSharper restore PossibleInvalidOperationException
             var actual = _uut.Statistic;
-            CollectionAssert.AreEqual(expected, actual);
+            CollectionAssert.IsNotEmpty(actual[developer1]);
+            CollectionAssert.IsNotEmpty(actual[developer2]);
         }
     }
 }
