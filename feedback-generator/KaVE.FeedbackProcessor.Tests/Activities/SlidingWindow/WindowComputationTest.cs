8f7f210032aef2d4b5b5fc16d4a47bc5c147f4a9
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-05-19 12:34:26 +0200

    Detecting windows from activity event streams.

---
+++ feedback-generator/KaVE.FeedbackProcessor.Tests/Activities/SlidingWindow/WindowComputationTest.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Sven Amann
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KaVE.FeedbackProcessor.Activities.Model;
+using KaVE.FeedbackProcessor.Activities.SlidingWindow;
+using KaVE.FeedbackProcessor.Tests.TestUtils;
+using NUnit.Framework;
+
+namespace KaVE.FeedbackProcessor.Tests.Activities.SlidingWindow
+{
+    [TestFixture]
+    internal class WindowComputationTest
+    {
+        [Test]
+        public void ComputesWindowFromStream()
+        {
+            var someDateTime = DateTimeFactory.SomeDateTime();
+            var event1 = GivenEventExists(someDateTime);
+            var event2 = GivenEventExists(someDateTime.AddSeconds(1));
+            var event3 = GivenEventExists(someDateTime.AddSeconds(2));
+            var event4 = GivenEventExists(someDateTime.AddSeconds(3));
+
+            var testMergeStrategy = new TestMergeStrategy();
+            var uut = new ActivityWindowProcessor(testMergeStrategy, TimeSpan.FromSeconds(3));
+            uut.OnEvent(event1);
+            uut.OnEvent(event2);
+            uut.OnEvent(event3);
+            uut.OnEvent(event4);
+            var window = testMergeStrategy.Windows.First();
+
+            CollectionAssert.AreEqual(ToActivities(event1, event2, event3), window);
+        }
+
+        private static ActivityEvent GivenEventExists(DateTime triggeredAt)
+        {
+            return new ActivityEvent {TriggeredAt = triggeredAt};
+        }
+
+        private static IEnumerable<Activity> ToActivities(params ActivityEvent[] events)
+        {
+            return events.Select(e => e.Activity);
+        }
+
+        private class TestMergeStrategy : ActivityWindowProcessor.IActivityMergeStrategy
+        {
+            public readonly IList<IList<Activity>> Windows = new List<IList<Activity>>();
+
+            public Activity Merge(IList<Activity> window)
+            {
+                Windows.Add(window);
+                return Activity.Any;
+            }
+        }
+    }
+}
---
+++ feedback-generator/KaVE.FeedbackProcessor/Activities/SlidingWindow/ActivityWindowProcessor.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Sven Amann
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KaVE.FeedbackProcessor.Activities.Model;
+
+namespace KaVE.FeedbackProcessor.Activities.SlidingWindow
+{
+    internal class ActivityWindowProcessor : BaseEventProcessor
+    {
+        private readonly IActivityMergeStrategy _strategy;
+        private readonly TimeSpan _windowSize;
+
+        private DateTime? WindowStart { get; set; }
+
+        private DateTime? WindowEnd
+        {
+            get { return WindowStart + _windowSize; }
+        }
+        private readonly IList<ActivityEvent> _window = new List<ActivityEvent>(); 
+
+        public interface IActivityMergeStrategy
+        {
+            Activity Merge(IList<Activity> window);
+        }
+
+        public ActivityWindowProcessor(IActivityMergeStrategy strategy, TimeSpan windowSize)
+        {
+            _strategy = strategy;
+            _windowSize = windowSize;
+            RegisterFor<ActivityEvent>(ProcessActivities);
+        }
+
+        private void ProcessActivities(ActivityEvent @event)
+        {
+            if (WindowEnd <= @event.TriggeredAt)
+            {
+                _strategy.Merge(_window.Select(e => e.Activity).ToList());
+                WindowStart = null;
+                _window.Clear();
+            }
+
+            if (WindowStart == null)
+            {
+                WindowStart = @event.TriggeredAt;
+            }
+            _window.Add(@event);
+        }
+    }
+}
