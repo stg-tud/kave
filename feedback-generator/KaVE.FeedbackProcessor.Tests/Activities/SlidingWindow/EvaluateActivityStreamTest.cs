37a1b2a180f577aed7d05e97afbcc232901a3743
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-05-20 11:53:03 +0200

    Evaluating activity streams by multiplying activity occurances with window span.

---
+++ feedback-generator/KaVE.FeedbackProcessor.Tests/Activities/SlidingWindow/EvaluateActivityStreamTest.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Sven Amann
+ */
+
+using System;
+using System.Collections.Generic;
+using KaVE.Commons.Utils.Collections;
+using KaVE.FeedbackProcessor.Activities.Model;
+using NUnit.Framework;
+
+namespace KaVE.FeedbackProcessor.Tests.Activities.SlidingWindow
+{
+    [TestFixture]
+    class EvaluateActivityStreamTest
+    {
+        private static readonly TimeSpan WindowSpan = TimeSpan.FromSeconds(2);
+
+        [Test]
+        public void CountsOccurrancesOfActivityTimesWindowSpan()
+        {
+            var statistic = Evaluate(new[] {Activity.Development, Activity.Development, Activity.Development}, WindowSpan);
+
+            Assert.AreEqual(statistic[Activity.Development], Times(WindowSpan,3));
+        }
+
+        private static TimeSpan Times(TimeSpan span, int factor)
+        {
+            return TimeSpan.FromTicks(factor*span.Ticks);
+        }
+
+        private IDictionary<Activity, TimeSpan> Evaluate(IEnumerable<Activity> stream, TimeSpan windowSpan)
+        {
+            var activities = new Multiset<Activity>(stream);
+            var statistic = new Dictionary<Activity, TimeSpan>();
+            foreach (var kvp in activities.EntryDictionary)
+            {
+                statistic[kvp.Key] = Times(windowSpan, kvp.Value);
+            }
+            return statistic;
+        }
+    }
+}
