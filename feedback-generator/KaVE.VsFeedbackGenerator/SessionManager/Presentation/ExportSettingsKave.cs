b9d0befc6fc528d6976b7c409c1164a8a99a2daa
markuszm <markus_zimmermann-ffm@t-online.de>
2015-06-05 18:15:40 +0200

    Added alternative ExportSettings file and parameter in Project File to switch between these two ExportSettings

---
+++ feedback-generator/KaVE.VsFeedbackGenerator/SessionManager/Presentation/ExportSettingsKave.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *    - Uli Fahrer
+ *    - Sven Amann
+ *    - Sebastian Proksch
+ */
+
+using JetBrains.Application.Settings;
+
+namespace KaVE.VsFeedbackGenerator.SessionManager.Presentation
+{
+    [SettingsKey(typeof(FeedbackSettings), "Kave Feedback-Export Settings")]
+    // WARNING: Do not change classname, as it is used to identify settings
+    internal class ExportSettings
+    {
+        [SettingsEntry(false, "KaVE FeedbackGeneration RemoveCodeNames")]
+        public bool RemoveCodeNames;
+
+        [SettingsEntry(false, "KaVE FeedbackGenerator RemoveDurations")]
+        public bool RemoveDurations;
+
+        [SettingsEntry(false, "KaVE FeedbackGenerator RemoveStartTimes")]
+        public bool RemoveStartTimes;
+
+        [SettingsEntry(false, "KaVE FeedbackGenerator RemoveSessionIDs")]
+        public bool RemoveSessionIDs;
+
+        [SettingsEntry("http://kave.st.informatik.tu-darmstadt.de:80/", "KaVE FeedbackGenerator UploadUrl")]
+        public string UploadUrl;
+
+        [SettingsEntry("", "KaVE FeedbackGenerator WebAccessPrefix")]
+        public string WebAccessPrefix;
+    }
+}
