23df81c09ba1ee780656f8f91b320a193b9a8699
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-07-16 00:46:17 +0200

    major cleanup (and ordering fix) for wrapped lookup items

---
+++ feedback-generator/KaVE.VS.FeedbackGenerator/CodeCompletion/AbstractWrappedLookupItem.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.Match;
+using JetBrains.ReSharper.Feature.Services.Lookup;
+using JetBrains.TextControl;
+using JetBrains.UI.Icons;
+using JetBrains.UI.RichText;
+using JetBrains.Util;
+
+namespace KaVE.VS.FeedbackGenerator.CodeCompletion
+{
+    public abstract class AbstractWrappedLookupItem : IWrappedLookupItem
+    {
+        public ILookupItem Item { get; protected set; }
+
+        public bool AcceptIfOnlyMatched(LookupItemAcceptanceContext itemAcceptanceContext)
+        {
+            return Item.AcceptIfOnlyMatched(itemAcceptanceContext);
+        }
+
+        public MatchingResult Match(PrefixMatcher prefixMatcher, ITextControl textControl)
+        {
+            return Item.Match(prefixMatcher, textControl);
+        }
+
+        public void Accept(ITextControl textControl,
+            TextRange nameRange,
+            LookupItemInsertType lookupItemInsertType,
+            Suffix suffix,
+            ISolution solution,
+            bool keepCaretStill)
+        {
+            Item.Accept(textControl, nameRange, lookupItemInsertType, suffix, solution, keepCaretStill);
+        }
+
+        public TextRange GetVisualReplaceRange(ITextControl textControl, TextRange nameRange)
+        {
+            return Item.GetVisualReplaceRange(textControl, nameRange);
+        }
+
+        public bool Shrink()
+        {
+            return Item.Shrink();
+        }
+
+        public void Unshrink()
+        {
+            Item.Unshrink();
+        }
+
+        public virtual LookupItemPlacement Placement
+        {
+            get { return Item.Placement; }
+            set { Item.Placement = value; }
+        }
+
+        public virtual IconId Image
+        {
+            get { return Item.Image; }
+        }
+
+        public virtual RichText DisplayName
+        {
+            get { return Item.DisplayName; }
+        }
+
+        public RichText DisplayTypeName
+        {
+            get { return Item.DisplayTypeName; }
+        }
+
+        public bool CanShrink
+        {
+            get { return Item.CanShrink; }
+        }
+
+        public int Multiplier
+        {
+            get { return Item.Multiplier; }
+            set { Item.Multiplier = value; }
+        }
+
+        public EvaluationMode Mode
+        {
+            get { return Item.Mode; }
+            set { Item.Mode = value; }
+        }
+
+        public bool IsDynamic
+        {
+            get { return Item.IsDynamic; }
+        }
+
+        public bool IgnoreSoftOnSpace
+        {
+            get { return Item.IgnoreSoftOnSpace; }
+            set { Item.IgnoreSoftOnSpace = value; }
+        }
+
+        public bool IsStable
+        {
+            get { return Item.IsStable; }
+            set { Item.IsStable = value; }
+        }
+
+        public string Identity
+        {
+            get { return Item.Identity; }
+        }
+    }
+}
--- feedback-generator/KaVE.VS.FeedbackGenerator/CodeCompletion/PBNProposalWrappedLookupItem.cs
+++ feedback-generator/KaVE.VS.FeedbackGenerator/CodeCompletion/PBNProposalWrappedLookupItem.cs
@@ -16,84 +16,25 @@
 
 using System;
 using System.Drawing;
-using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Feature.Services.CodeCompletion;
 using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
-using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.Match;
-using JetBrains.ReSharper.Feature.Services.Lookup;
-using JetBrains.TextControl;
 using JetBrains.UI.Icons;
 using JetBrains.UI.Icons.ColorIcons;
 using JetBrains.UI.Icons.ComposedIcons;
 using JetBrains.UI.RichText;
-using JetBrains.Util;
 
 namespace KaVE.VS.FeedbackGenerator.CodeCompletion
 {
-    internal class PBNProposalWrappedLookupItem : IWrappedLookupItem
+    internal class PBNProposalWrappedLookupItem : AbstractWrappedLookupItem
     {
-        private readonly ILookupItem _wrappedItem;
-
         private readonly int _probability;
 
         public PBNProposalWrappedLookupItem(ILookupItem wrappedItem, double probability)
         {
-            _wrappedItem = wrappedItem;
+            Item = wrappedItem;
             _probability = (int) (probability*100);
         }
 
-        public RichText DisplayName
-        {
-            get
-            {
-                var original = _wrappedItem.DisplayName.Clone();
-                original.SetStyle(FontStyle.Bold);
-                var probStyle = new TextStyle {ForegroundColor = Color.Gray};
-                return original.Append(" (" + _probability + "%)", probStyle);
-            }
-        }
-
-        public IconId Image
-        {
-            get { return CompositeIconId.Compose(new ColorIconId(Color.Gold), _wrappedItem.Image); }
-        }
-
-        public bool AcceptIfOnlyMatched(LookupItemAcceptanceContext itemAcceptanceContext)
-        {
-            return _wrappedItem.AcceptIfOnlyMatched(itemAcceptanceContext);
-        }
-
-        public MatchingResult Match(PrefixMatcher prefixMatcher, ITextControl textControl)
-        {
-            return _wrappedItem.Match(prefixMatcher, textControl);
-        }
-
-        public void Accept(ITextControl textControl,
-            TextRange nameRange,
-            LookupItemInsertType lookupItemInsertType,
-            Suffix suffix,
-            ISolution solution,
-            bool keepCaretStill)
-        {
-            _wrappedItem.Accept(textControl, nameRange, lookupItemInsertType, suffix, solution, keepCaretStill);
-        }
-
-        public TextRange GetVisualReplaceRange(ITextControl textControl, TextRange nameRange)
-        {
-            return _wrappedItem.GetVisualReplaceRange(textControl, nameRange);
-        }
-
-        public bool Shrink()
-        {
-            return _wrappedItem.Shrink();
-        }
-
-        public void Unshrink()
-        {
-            _wrappedItem.Unshrink();
-        }
-
-        public LookupItemPlacement Placement
+        public override LookupItemPlacement Placement
         {
             get
             {
@@ -102,8 +43,8 @@ namespace KaVE.VS.FeedbackGenerator.CodeCompletion
                     PlacementLocation.Top,
                     SelectionPriority.High)
                 {
-                    Relevance = _wrappedItem.Placement.Relevance,
-                    Rank = _wrappedItem.Placement.Rank
+                    Relevance = long.MaxValue,
+                    Rank = Item.Placement.Rank
                 };
                 return p;
             }
@@ -114,59 +55,27 @@ namespace KaVE.VS.FeedbackGenerator.CodeCompletion
         {
             get
             {
-                var inverseProbability = 100 - _probability;
-                var orderingPrefix = "0";
-                if (inverseProbability < 100)
-                {
-                    orderingPrefix = "00";
-                }
-                else if (inverseProbability < 10)
-                {
-                    orderingPrefix = "000";
-                }
-                return orderingPrefix + inverseProbability + _wrappedItem.Placement.OrderString;
+                return string.Format(
+                    "{0:000}_{1}",
+                    100 - _probability,
+                    Item.Placement.OrderString);
             }
         }
 
-        public RichText DisplayTypeName
+        public override IconId Image
         {
-            get { return _wrappedItem.DisplayTypeName; }
+            get { return CompositeIconId.Compose(new ColorIconId(Color.Gold), Item.Image); }
         }
 
-        public bool CanShrink
+        public override RichText DisplayName
         {
-            get { return _wrappedItem.CanShrink; }
-        }
-
-        public int Multiplier
-        {
-            get { return _wrappedItem.Multiplier; }
-            set { _wrappedItem.Multiplier = value; }
-        }
-
-        public EvaluationMode Mode { get; set; }
-
-        public bool IsDynamic
-        {
-            get { return _wrappedItem.IsDynamic; }
-        }
-
-        public bool IgnoreSoftOnSpace
-        {
-            get { return _wrappedItem.IgnoreSoftOnSpace; }
-            set { _wrappedItem.IgnoreSoftOnSpace = value; }
-        }
-
-        public bool IsStable { get; set; }
-
-        public string Identity
-        {
-            get { return _wrappedItem.Identity; }
-        }
-
-        public ILookupItem Item
-        {
-            get { return _wrappedItem; }
+            get
+            {
+                var original = Item.DisplayName.Clone();
+                original.SetStyle(FontStyle.Bold);
+                var probStyle = new TextStyle {ForegroundColor = Color.Gray};
+                return original.Append(" (" + _probability + "%)", probStyle);
+            }
         }
     }
 }
