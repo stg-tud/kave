ede27c29db1f41b7e52c095407d350005ee06e17
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-07-27 12:38:26 +0200

    increased cache timeout

--- feedback-generator/KaVE.RS.Commons/Analysis/ContextAnalysis.cs
+++ feedback-generator/KaVE.RS.Commons/Analysis/ContextAnalysis.cs
@@ -37,7 +37,7 @@ namespace KaVE.RS.Commons.Analysis
     public class ContextAnalysis
     {
         public const int DefaultTimeLimitInMs = 1000;
-        public const int CacheTimeout = 5000;
+        public const int CacheTimeout = 10000;
 
         private static readonly object Lock = new object();
         private static readonly KaVECancellationTokenSource TokenSource = new KaVECancellationTokenSource();
