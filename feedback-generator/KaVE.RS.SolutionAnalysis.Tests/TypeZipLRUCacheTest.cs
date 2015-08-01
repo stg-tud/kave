2e2e71e7fe2ead07093a21d7c82cd4c4be2146b3
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-08-01 21:35:16 +0200

    implemented LRU Cache for writing usages to .zip files

---
+++ feedback-generator/KaVE.RS.SolutionAnalysis.Tests/TypeZipLRUCacheTest.cs
@@ -0,0 +1,162 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.IO;
+using KaVE.Commons.Utils.Assertion;
+using NUnit.Framework;
+
+namespace KaVE.RS.SolutionAnalysis.Tests
+{
+    // ReSharper disable ObjectCreationAsStatement
+    internal class TypeZipLRUCacheTest
+    {
+        private string _root;
+        private TypeZipLRUCache<string> _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _root = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(_root);
+            _sut = new TypeZipLRUCache<string>(_root, 2);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Directory.Delete(_root, true);
+        }
+
+        [Test]
+        public void SizeAndCache()
+        {
+            Assert.AreEqual(0, _sut.Size);
+            Assert.False(_sut.IsCached("a"));
+
+            _sut.GetArchive("a");
+
+            Assert.AreEqual(1, _sut.Size);
+            Assert.True(_sut.IsCached("a"));
+        }
+
+        [Test]
+        public void FilesAreCreatedInCorrectSubfolders()
+        {
+            using (var wa = _sut.GetArchive("a"))
+            {
+                wa.Add("something");
+            }
+
+            Assert.True(Directory.Exists(_root));
+            Assert.True(File.Exists(Path.Combine(_root, "a/0.zip")));
+        }
+
+        [Test]
+        public void ReplacementInKeysWorks()
+        {
+            var a = @"aA1._!";
+            var e = @"aA1.__\0.zip";
+            using (var wa = _sut.GetArchive(a))
+            {
+                wa.Add("something");
+            }
+
+            Assert.True(Directory.Exists(_root));
+            Assert.True(File.Exists(Path.Combine(_root, e)));
+        }
+
+        [Test]
+        public void CacheDoesNotGrowLargerThanCapacity()
+        {
+            _sut.GetArchive("a");
+            _sut.GetArchive("b");
+            _sut.GetArchive("c");
+
+            Assert.AreEqual(2, _sut.Size);
+        }
+
+        [Test]
+        public void LeastRecentlyUsedKeyIsRemoved()
+        {
+            _sut.GetArchive("a");
+            _sut.GetArchive("b");
+            _sut.GetArchive("c");
+
+            Assert.False(_sut.IsCached("a"));
+            Assert.True(_sut.IsCached("b"));
+            Assert.True(_sut.IsCached("c"));
+        }
+
+        [Test]
+        public void RefreshingWorks()
+        {
+            _sut.GetArchive("a");
+            _sut.GetArchive("b");
+            _sut.GetArchive("a");
+            _sut.GetArchive("c");
+
+            Assert.False(_sut.IsCached("b"));
+            Assert.True(_sut.IsCached("a"));
+            Assert.True(_sut.IsCached("c"));
+        }
+
+        [Test]
+        public void CacheRemoveClosesOpenArchive()
+        {
+            var expectedFileName = Path.Combine(_root, "a/0.zip");
+
+            var wa = _sut.GetArchive("a");
+            wa.Add("something");
+            _sut.GetArchive("b");
+
+            Assert.False(File.Exists(expectedFileName));
+            _sut.GetArchive("c");
+            Assert.True(File.Exists(expectedFileName));
+        }
+
+        [Test]
+        public void DisposeClosesAllOpenArchives()
+        {
+            var wa = _sut.GetArchive("a");
+            wa.Add("something");
+
+            _sut.Dispose();
+
+            Assert.False(_sut.IsCached("a"));
+            Assert.AreEqual(0, _sut.Size);
+            Assert.True(File.Exists(Path.Combine(_root, "a/0.zip")));
+        }
+
+
+        [Test, ExpectedException(typeof (AssertException))]
+        public void DirectoryHasToExist()
+        {
+            new TypeZipLRUCache<string>(@"c:\does\not\exist\", 10);
+        }
+
+        [Test, ExpectedException(typeof (AssertException))]
+        public void DirectoryMustNotBeAFile()
+        {
+            new TypeZipLRUCache<string>(@"c:\Windows\notepad.exe", 10);
+        }
+
+        [Test, ExpectedException(typeof (AssertException))]
+        public void CapacityMustBeLargerThanZero()
+        {
+            new TypeZipLRUCache<string>(Path.GetTempPath(), 0);
+        }
+    }
+}
---
+++ feedback-generator/KaVE.RS.SolutionAnalysis/TypeZipLRUCache.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using KaVE.Commons.Utils.Assertion;
+using KaVE.Commons.Utils.IO;
+
+namespace KaVE.RS.SolutionAnalysis
+{
+    public class TypeZipLRUCache<K> : IDisposable where K : class
+    {
+        private readonly string _root;
+        private readonly int _capacity;
+
+        private readonly List<K> _accessOrderList = new List<K>();
+        private readonly Dictionary<K, IWritingArchive> _openArchives = new Dictionary<K, IWritingArchive>();
+        private readonly Dictionary<K, TypeZipFolder> _folders = new Dictionary<K, TypeZipFolder>();
+
+
+        public TypeZipLRUCache(string root, int capacity)
+        {
+            Asserts.That(Directory.Exists(root));
+            Asserts.That(capacity > 0);
+
+            _root = root;
+            _capacity = capacity;
+        }
+
+        public IWritingArchive GetArchive(K type)
+        {
+            RefreshAccess(type);
+
+            if (_openArchives.ContainsKey(type))
+            {
+                return _openArchives[type];
+            }
+
+            var folder = _folders.ContainsKey(type)
+                ? _folders[type]
+                : GetFolder(type);
+
+            var wa = folder.CreateNewArchive();
+            _openArchives.Add(type, wa);
+
+            EnsureCapacityIsRespected();
+
+            return wa;
+        }
+
+        private void EnsureCapacityIsRespected()
+        {
+            if (_accessOrderList.Count > _capacity)
+            {
+                var oldest = _accessOrderList.First();
+                _accessOrderList.Remove(oldest);
+                _openArchives[oldest].Dispose();
+                _openArchives.Remove(oldest);
+            }
+        }
+
+        private void RefreshAccess(K type)
+        {
+            _accessOrderList.Remove(type);
+            _accessOrderList.Add(type);
+        }
+
+        private TypeZipFolder GetFolder(K type)
+        {
+            if (_folders.ContainsKey(type))
+            {
+                return _folders[type];
+            }
+
+            var folderName = GetTargetFolder(type);
+            if (!Directory.Exists(folderName))
+            {
+                Directory.CreateDirectory(folderName);
+            }
+            var folderUtil = new TypeZipFolder(folderName);
+
+            _folders[type] = folderUtil;
+
+            return folderUtil;
+        }
+
+        private string GetTargetFolder(K type)
+        {
+            var regex = new Regex(@"[^a-zA-Z0-9._]");
+            var relativeFolderName = type.ToString();
+            relativeFolderName = regex.Replace(relativeFolderName, "_");
+            return Path.Combine(_root, relativeFolderName);
+        }
+
+        public bool IsCached(K type)
+        {
+            return _openArchives.ContainsKey(type);
+        }
+
+        public int Size
+        {
+            get { return _openArchives.Count; }
+        }
+
+        public void Dispose()
+        {
+            foreach (var wa in _openArchives.Values)
+            {
+                wa.Dispose();
+            }
+            _accessOrderList.Clear();
+            _openArchives.Clear();
+            _folders.Clear();
+        }
+    }
+}
