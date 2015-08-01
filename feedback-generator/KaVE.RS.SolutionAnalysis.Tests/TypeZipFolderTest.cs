39cd2f9b17d26d8248de302896b81d6084cbaa20
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-08-01 19:14:58 +0200

    implemented util for reading folders that includes multiple Zips for a single type...

---
+++ feedback-generator/KaVE.RS.SolutionAnalysis.Tests/TypeZipFolderTest.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.IO;
+using KaVE.Commons.Utils.Assertion;
+using NUnit.Framework;
+
+namespace KaVE.RS.SolutionAnalysis.Tests
+{
+    // ReSharper disable ObjectCreationAsStatement
+    internal class TypeZipFolderTest
+    {
+        private string _root;
+        private TypeZipFolder _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _root = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(_root);
+            _sut = new TypeZipFolder(_root);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Directory.Delete(_root, true);
+        }
+
+        [Test]
+        public void ArchivesAreCreatedOnRequest()
+        {
+            Assert.AreEqual(0, _sut.NumArchives());
+
+            using (var wa = _sut.CreateNewArchive())
+            {
+                wa.Add("a");
+                wa.Add("b");
+            }
+
+            Assert.AreEqual(1, _sut.NumArchives());
+
+            using (var wa = _sut.CreateNewArchive())
+            {
+                wa.Add("c");
+                wa.Add("d");
+            }
+
+            Assert.AreEqual(2, _sut.NumArchives());
+        }
+
+        [Test]
+        public void NoFilesAreOverwritten()
+        {
+            using (File.Create(Path.Combine(_root, "0.zip"))) {}
+            using (File.Create(Path.Combine(_root, "1.zip"))) {}
+
+            Assert.AreEqual(2, _sut.NumArchives());
+
+            using (var wa = _sut.CreateNewArchive())
+            {
+                wa.Add("a");
+            }
+
+            Assert.AreEqual(3, _sut.NumArchives());
+        }
+
+        [Test]
+        public void CorrectContentIsWrittenAndRead()
+        {
+            using (var wa = _sut.CreateNewArchive())
+            {
+                wa.Add("a");
+                wa.Add("b");
+            }
+            using (var wa = _sut.CreateNewArchive())
+            {
+                wa.Add("c");
+                wa.Add("d");
+            }
+
+            var ras = _sut.OpenAll();
+            Assert.AreEqual(2, ras.Count);
+
+            foreach (var ra in ras)
+            {
+                var entries = ra.GetAll<string>();
+
+                Assert.AreEqual(2, entries.Count);
+
+                if (entries.Contains("a"))
+                {
+                    Assert.True(entries.Contains("b"));
+                }
+                else
+                {
+                    Assert.True(entries.Contains("c"));
+                    Assert.True(entries.Contains("d"));
+                }
+            }
+        }
+
+        [Test, ExpectedException(typeof (AssertException))]
+        public void DirectoryHasToExist()
+        {
+            new TypeZipFolder(@"c:\does\not\exist\");
+        }
+
+        [Test, ExpectedException(typeof (AssertException))]
+        public void DirectoryMustNotBeAFile()
+        {
+            new TypeZipFolder(@"c:\Windows\notepad.exe");
+        }
+    }
+}
---
+++ feedback-generator/KaVE.RS.SolutionAnalysis/TypeZipFolder.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using KaVE.Commons.Utils.Assertion;
+using KaVE.Commons.Utils.IO;
+
+namespace KaVE.RS.SolutionAnalysis
+{
+    public class TypeZipFolder
+    {
+        private readonly string _root;
+        private int _fileCounter;
+
+        public TypeZipFolder(string root)
+        {
+            Asserts.That(Directory.Exists(root));
+            _root = root;
+        }
+
+        public IWritingArchive CreateNewArchive()
+        {
+            var fileName = CreateNextUnusedFileName();
+            return new WritingArchive(fileName);
+        }
+
+        private string CreateNextUnusedFileName()
+        {
+            var fileName = CreateNextFileName();
+            while (File.Exists(fileName))
+            {
+                fileName = CreateNextFileName();
+            }
+            return fileName;
+        }
+
+        private string CreateNextFileName()
+        {
+            return Path.Combine(_root, string.Format("{0}.zip", _fileCounter++));
+        }
+
+        public int NumArchives()
+        {
+            return Directory.EnumerateFiles(_root, "*.zip").Count();
+        }
+
+        public IList<IReadingArchive> OpenAll()
+        {
+            return Directory.EnumerateFiles(_root, "*.zip").Select(Open).ToList();
+        }
+
+        public IReadingArchive Open(string path)
+        {
+            return new ReadingArchive(path);
+        }
+    }
+}
