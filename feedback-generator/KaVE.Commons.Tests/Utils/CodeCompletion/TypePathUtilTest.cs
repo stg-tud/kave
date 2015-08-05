a360bc6a7e8fab5c6bdf32a1fa9e4b6b1963dfde
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-08-05 23:05:19 +0200

    added utils to convert types (or any other object) to nested or flat paths

---
+++ feedback-generator/KaVE.Commons.Tests/Utils/CodeCompletion/TypePathUtilTest.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using KaVE.Commons.Model.ObjectUsage;
+using KaVE.Commons.Utils.CodeCompletion.Impl;
+using NUnit.Framework;
+
+namespace KaVE.Commons.Tests.Utils.CodeCompletion
+{
+    internal class TypePathUtilTest
+    {
+        private TypePathUtil _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new TypePathUtil();
+        }
+
+        [Test]
+        public void TypeAsNested()
+        {
+            var name = new CoReTypeName("La/b/C");
+            var actual = _sut.ToNestedPath(name);
+            const string expected = "La/b/C";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TypeAsFlat()
+        {
+            var name = new CoReTypeName("La/b/C");
+            var actual = _sut.ToFlatPath(name);
+            const string expected = "La_b_C";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ feedback-generator/KaVE.Commons/Utils/CodeCompletion/Impl/TypePathUtil.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Text.RegularExpressions;
+using KaVE.Commons.Utils.Json;
+
+namespace KaVE.Commons.Utils.CodeCompletion.Impl
+{
+    public class TypePathUtil
+    {
+        public string ToNestedPath(object o)
+        {
+            var regex = new Regex(@"[^a-zA-Z0-9,\-_/+$(){}[\]]");
+
+            var relName = o.ToCompactJson();
+            relName = relName.Replace('.', '/');
+            relName = relName.Replace("\\\"", "\""); // quotes inside json
+            relName = relName.Replace("\"", ""); // surrounding quotes
+            relName = relName.Replace('\\', '/');
+            relName = regex.Replace(relName, "_");
+
+            return relName;
+        }
+
+        public string ToFlatPath(object o)
+        {
+            var nested = ToNestedPath(o);
+            var flat = nested.Replace("/", "_");
+            return flat;
+        }
+    }
+}
