34ee9d07f956b0d6ab8a55603b34758627d199de
M8is <M8is@live.de>
2015-09-09 18:04:05 +0200

    Added missing license headers.

--- feedback-generator/KaVE.Commons.Tests/Model/SSTs/Impl/Visitor/CompletionPrefixVisitorTest.cs
+++ feedback-generator/KaVE.Commons.Tests/Model/SSTs/Impl/Visitor/CompletionPrefixVisitorTest.cs
@@ -1,4 +1,20 @@
-﻿using System.Text;
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Text;
 using KaVE.Commons.Model.SSTs;
 using KaVE.Commons.Model.SSTs.Impl;
 using KaVE.Commons.Model.SSTs.Impl.Declarations;
@@ -18,7 +34,7 @@ namespace KaVE.Commons.Tests.Model.SSTs.Impl.Visitor
             const string testPrefix = "SomeObj";
 
             var context = new StringBuilder();
-            
+
             var sst = new SST();
             var methodDeclarationContainingCompletionExpression = new MethodDeclaration
             {
@@ -30,7 +46,7 @@ namespace KaVE.Commons.Tests.Model.SSTs.Impl.Visitor
                         })
             };
             sst.Methods.Add(methodDeclarationContainingCompletionExpression);
-            
+
             sst.Accept(new CompletionPrefixVisitor(), context);
 
             Assert.AreEqual(testPrefix, context.ToString());
