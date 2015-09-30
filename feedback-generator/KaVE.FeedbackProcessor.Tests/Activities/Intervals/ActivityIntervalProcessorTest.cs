f5075e127a86313840ac0d4da05d912f7ad59950
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-09-30 15:58:23 +0200

    Computing intervals from Activity stream.

---
+++ feedback-generator/KaVE.FeedbackProcessor.Tests/Activities/Intervals/ActivityIntervalProcessorTest.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KaVE.FeedbackProcessor.Activities.Intervals;
+using KaVE.FeedbackProcessor.Activities.Model;
+using KaVE.FeedbackProcessor.Model;
+using KaVE.FeedbackProcessor.Tests.Model;
+using KaVE.FeedbackProcessor.Tests.TestUtils;
+using NUnit.Framework;
+
+namespace KaVE.FeedbackProcessor.Tests.Activities.Intervals
+{
+    [TestFixture]
+    class ActivityIntervalProcessorTest
+    {
+        private ActivityIntervalProcessor _uut;
+        private DateTime _someDateTime;
+        private Developer _someDeveloper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new ActivityIntervalProcessor();
+            _someDateTime = DateTimeFactory.SomeWorkingHoursDateTime();
+            _someDeveloper = TestFactory.SomeDeveloper();
+        }
+
+        [Test]
+        public void Interval()
+        {
+            _uut.OnStreamStarts(_someDeveloper);
+            _uut.OnEvent(SomeEvent(_someDateTime, Activity.Other));
+            _uut.OnEvent(SomeEvent(_someDateTime.AddSeconds(3), Activity.Other, TimeSpan.FromSeconds(1)));
+            _uut.OnStreamEnds();
+
+            AssertIntervals(_uut.Intervals[_someDeveloper],
+                Interval(_someDateTime, Activity.Other, TimeSpan.FromSeconds(4)));
+        }
+
+        [Test]
+        public void IntervalWithoutEnd()
+        {
+            _uut.OnStreamStarts(_someDeveloper);
+            _uut.OnEvent(SomeEvent(_someDateTime, Activity.Other));
+            _uut.OnEvent(SomeEvent(_someDateTime.AddSeconds(3), Activity.Other));
+            _uut.OnStreamEnds();
+
+            AssertIntervals(_uut.Intervals[_someDeveloper],
+                Interval(_someDateTime, Activity.Other, TimeSpan.FromSeconds(3)));
+        }
+
+        [Test]
+        public void Intervals()
+        {
+            _uut.OnStreamStarts(_someDeveloper);
+            _uut.OnEvent(SomeEvent(_someDateTime, Activity.Other, TimeSpan.FromSeconds(1)));
+            _uut.OnEvent(SomeEvent(_someDateTime.AddSeconds(3), Activity.Navigation, TimeSpan.FromSeconds(2)));
+            _uut.OnStreamEnds();
+
+            AssertIntervals(_uut.Intervals[_someDeveloper],
+                Interval(_someDateTime, Activity.Other, TimeSpan.FromSeconds(1)),
+                Interval(_someDateTime.AddSeconds(3), Activity.Navigation, TimeSpan.FromSeconds(2)));
+        }
+
+        private static void AssertIntervals(IList<ActivityIntervalProcessor.Interval> actuals, params ActivityIntervalProcessor.Interval[] expecteds)
+        {
+            Assert.AreEqual(expecteds, actuals);
+        }
+
+        private static ActivityIntervalProcessor.Interval Interval(DateTime start, Activity activity, TimeSpan duration)
+        {
+            return new ActivityIntervalProcessor.Interval
+            {
+                Start = start,
+                Activity = activity,
+                End = start + duration
+            };
+        }
+
+        private static ActivityEvent SomeEvent(DateTime triggeredAt, Activity activity)
+        {
+            return new ActivityEvent { TriggeredAt = triggeredAt, Activity = activity};
+        }
+
+        private static ActivityEvent SomeEvent(DateTime triggeredAt, Activity activity, TimeSpan duration)
+        {
+            return new ActivityEvent { TriggeredAt = triggeredAt, Activity = activity, Duration = duration };
+        }
+    }
+}
---
+++ feedback-generator/KaVE.FeedbackProcessor/Activities/Intervals/ActivityIntervalProcessor.cs
@@ -0,0 +1,122 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using KaVE.Commons.Utils;
+using KaVE.FeedbackProcessor.Activities.Model;
+using KaVE.FeedbackProcessor.Model;
+
+namespace KaVE.FeedbackProcessor.Activities.Intervals
+{
+    internal class ActivityIntervalProcessor : BaseEventProcessor
+    {
+        public class Interval
+        {
+            public Activity Activity { get; internal set; }
+            public DateTime Start { get; internal set; }
+            public DateTime End { get; internal set; }
+
+            protected bool Equals(Interval other)
+            {
+                return Activity == other.Activity && Start.Equals(other.Start) && End.Equals(other.End);
+            }
+
+            public override bool Equals(object obj)
+            {
+                return this.Equals(obj, Equals);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    var hashCode = (int) Activity;
+                    hashCode = (hashCode*397) ^ Start.GetHashCode();
+                    hashCode = (hashCode*397) ^ End.GetHashCode();
+                    return hashCode;
+                }
+            }
+
+            public override string ToString()
+            {
+                return string.Format("Start: {1}, Activity: {0}, End: {2}", Activity, Start, End);
+            }
+        }
+
+        public IDictionary<Developer, IList<Interval>> Intervals;
+        private Developer _currentDeveloper;
+        private Interval _currentInterval;
+        private readonly TimeSpan _activityTimeout;
+
+        public ActivityIntervalProcessor()
+        {
+            Intervals = new Dictionary<Developer, IList<Interval>>();
+            RegisterFor<ActivityEvent>(Handle);
+            _activityTimeout = TimeSpan.FromMilliseconds(16000);
+        }
+
+        public override void OnStreamStarts(Developer developer)
+        {
+            _currentDeveloper = developer;
+            Intervals[developer] = new List<Interval>();
+            _currentInterval = null;
+        }
+
+        private void Handle(ActivityEvent @event)
+        {
+            if (HasNoOpenInterval())
+            {
+                StartIntervalAt(@event);
+            }
+
+            if (_currentInterval.Activity != @event.Activity)
+            {
+                StartIntervalAt(@event);
+            }
+            else
+            {
+                _currentInterval.End = GetEnd(@event);
+            }
+        }
+
+        private static DateTime GetEnd(ActivityEvent @event)
+        {
+            return @event.TerminatedAt ?? @event.GetTriggeredAt();
+        }
+
+        private bool HasNoOpenInterval()
+        {
+            return _currentInterval == null;
+        }
+
+        private bool OpenIntervalTimedOut(ActivityEvent @event)
+        {
+            return _currentInterval.End + _activityTimeout < @event.GetTriggeredAt();
+        }
+
+        private void StartIntervalAt(ActivityEvent @event)
+        {
+            _currentInterval = new Interval
+            {
+                Start = @event.GetTriggeredAt(),
+                Activity = @event.Activity,
+                End = GetEnd(@event)
+            };
+            Intervals[_currentDeveloper].Add(_currentInterval);
+        }
+    }
+}
