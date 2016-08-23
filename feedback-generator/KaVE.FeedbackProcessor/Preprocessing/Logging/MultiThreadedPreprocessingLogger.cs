16ca7881d95262b86f82809bb28924dd3eb6984c
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-08-23 13:52:00 +0200

    Make MultiThreadedPreprocessingLogger thread-safe.

--- feedback-generator/KaVE.FeedbackProcessor/Preprocessing/Logging/MultiThreadedPreprocessingLogger.cs
+++ feedback-generator/KaVE.FeedbackProcessor/Preprocessing/Logging/MultiThreadedPreprocessingLogger.cs
@@ -44,6 +44,7 @@ namespace KaVE.FeedbackProcessor.Preprocessing.Logging
 
     public class MultiThreadedPreprocessingLogger : IMultiThreadedPreprocessingLogger
     {
+        private readonly object _lock = new object();
         private readonly IPrepocessingLogger _log;
 
         public MultiThreadedPreprocessingLogger(IPrepocessingLogger log)
@@ -53,14 +54,17 @@ namespace KaVE.FeedbackProcessor.Preprocessing.Logging
 
         public void Init(int numWorkers, string dirIn, string dirMerged, string dirOut)
         {
-            _log.Log(new string('#', 60));
-            _log.Log("# MultiThreadedPreprocessing");
-            _log.Log(new string('#', 60));
-            _log.Log();
-            _log.Log("workers: {0}", numWorkers);
-            _log.Log("dirIn: {0}", dirIn);
-            _log.Log("dirMerged: {0}", dirMerged);
-            _log.Log("dirOut: {0}", dirOut);
+            lock (_lock)
+            {
+                _log.Log(new string('#', 60));
+                _log.Log("# MultiThreadedPreprocessing");
+                _log.Log(new string('#', 60));
+                _log.Log();
+                _log.Log("workers: {0}", numWorkers);
+                _log.Log("dirIn: {0}", dirIn);
+                _log.Log("dirMerged: {0}", dirMerged);
+                _log.Log("dirOut: {0}", dirOut);
+            }
         }
 
         private int _numZips;
@@ -68,37 +72,52 @@ namespace KaVE.FeedbackProcessor.Preprocessing.Logging
 
         public void ReadingIds(int numUnindexedZips)
         {
-            _numZips = numUnindexedZips;
+            lock (_lock)
+            {
+                _numZips = numUnindexedZips;
 
-            _log.Log();
-            _log.Log(new string('-', 60));
-            _log.Log("Reading ids from {0} zips", numUnindexedZips);
-            _log.Log(new string('-', 60));
+                _log.Log();
+                _log.Log(new string('-', 60));
+                _log.Log("Reading ids from {0} zips", numUnindexedZips);
+                _log.Log(new string('-', 60));
+            }
         }
 
         public void StartWorkerReadIds(int taskId)
         {
-            _log.Log("({0}) Starting worker", taskId);
+            lock (_lock)
+            {
+                _log.Log("({0}) Starting worker", taskId);
+            }
         }
 
         public void ReadIds(int taskId, string zip)
         {
-            _curZip++;
-            var perc = 100*_curZip/(double) _numZips;
-            _log.Log("({0}) Reading zip {1}/{2} ({3:0.00}% started): {4}", taskId, _curZip, _numZips, perc, zip);
+            lock (_lock)
+            {
+                _curZip++;
+                var perc = 100*_curZip/(double) _numZips;
+                _log.Log("({0}) Reading zip {1}/{2} ({3:0.00}% started): {4}", taskId, _curZip, _numZips, perc, zip);
+            }
         }
 
         public void StopWorkerReadIds(int taskId)
         {
-            _log.Log("({0}) Stopping worker", taskId);
+            lock (_lock)
+            {
+                _log.Log("({0}) Stopping worker", taskId);
+            }
         }
 
         public void GroupZipsByIds()
         {
-            _log.Log();
-            _log.Log(new string('-', 60));
-            _log.Log("GroupZipsByIds");
-            _log.Log(new string('-', 60));
+            lock (_lock)
+            {
+                _log.Log();
+                _log.Log(new string('-', 60));
+                _log.Log("GroupZipsByIds");
+                _log.Log(new string('-', 60));
+            }
         }
 
         private int _numGroups;
@@ -106,35 +125,47 @@ namespace KaVE.FeedbackProcessor.Preprocessing.Logging
 
         public void MergeGroups(int numGroups)
         {
-            _numGroups = numGroups;
+            lock (_lock)
+            {
+                _numGroups = numGroups;
 
-            _log.Log();
-            _log.Log(new string('-', 60));
-            _log.Log("Merging {0} groups", numGroups);
-            _log.Log(new string('-', 60));
+                _log.Log();
+                _log.Log(new string('-', 60));
+                _log.Log("Merging {0} groups", numGroups);
+                _log.Log(new string('-', 60));
+            }
         }
 
         public void StartWorkerMergeGroup(int taskId)
         {
-            _log.Log("({0}) Starting worker", taskId);
+            lock (_lock)
+            {
+                _log.Log("({0}) Starting worker", taskId);
+            }
         }
 
         public void MergeGroup(int taskId, int count)
         {
-            _curGroup++;
-            var perc = 100*_curGroup/(double) _numGroups;
-            _log.Log(
-                "({0}) Merging group {1}/{2} ({3:0.0}% started), contains {4} zips",
-                taskId,
-                _curGroup,
-                _numGroups,
-                perc,
-                count);
+            lock (_lock)
+            {
+                _curGroup++;
+                var perc = 100*_curGroup/(double) _numGroups;
+                _log.Log(
+                    "({0}) Merging group {1}/{2} ({3:0.0}% started), contains {4} zips",
+                    taskId,
+                    _curGroup,
+                    _numGroups,
+                    perc,
+                    count);
+            }
         }
 
         public void StopWorkerMergeGroup(int taskId)
         {
-            _log.Log("({0}) Stopping worker", taskId);
+            lock (_lock)
+            {
+                _log.Log("({0}) Stopping worker", taskId);
+            }
         }
 
         private int _numUnclean;
@@ -142,44 +173,63 @@ namespace KaVE.FeedbackProcessor.Preprocessing.Logging
 
         public void Cleaning(int numUnclean)
         {
-            _numUnclean = numUnclean;
+            lock (_lock)
+            {
+                _numUnclean = numUnclean;
 
-            _log.Log();
-            _log.Log(new string('-', 60));
-            _log.Log("Cleaning {0} zips", numUnclean);
-            _log.Log(new string('-', 60));
+                _log.Log();
+                _log.Log(new string('-', 60));
+                _log.Log("Cleaning {0} zips", numUnclean);
+                _log.Log(new string('-', 60));
+            }
         }
 
         public void StartWorkerCleanZip(int taskId)
         {
-            _log.Log("({0}) Starting worker", taskId);
+            lock (_lock)
+            {
+                _log.Log("({0}) Starting worker", taskId);
+            }
         }
 
         public void CleanZip(int taskId, string zip)
         {
-            _curUnclean++;
-            var perc = 100*_curUnclean/(double) _numUnclean;
-            _log.Log("({0}) Cleaning zip {1}/{2} ({3:0.0}% started): {4}", taskId, _curUnclean, _numUnclean, perc, zip);
+            lock (_lock)
+            {
+                _curUnclean++;
+                var perc = 100*_curUnclean/(double) _numUnclean;
+                _log.Log(
+                    "({0}) Cleaning zip {1}/{2} ({3:0.0}% started): {4}",
+                    taskId,
+                    _curUnclean,
+                    _numUnclean,
+                    perc,
+                    zip);
+            }
         }
 
         public void StopWorkerCleanZip(int taskId)
         {
-            _log.Log("({0}) Stopping worker", taskId);
+            lock (_lock)
+            {
+                _log.Log("({0}) Stopping worker", taskId);
+            }
         }
 
         public void Error(int taskId, Exception e)
         {
-            _log.Log(new string('#', 60));
-            _log.Log("# exception for worker {0}: {1}", taskId, e.Message);
-
-            var frames = e.StackTrace.Replace("\r\n", "\n").Split('\n');
-            foreach (var frame in frames)
+            lock (_lock)
             {
-                _log.Log("# {0}", frame);
-            }
-            _log.Log(new string('#', 60));
-        }
+                _log.Log(new string('#', 60));
+                _log.Log("# exception for worker {0}: {1}", taskId, e.Message);
 
-        // TODO add lock!!
+                var frames = e.StackTrace.Replace("\r\n", "\n").Split('\n');
+                foreach (var frame in frames)
+                {
+                    _log.Log("# {0}", frame);
+                }
+                _log.Log(new string('#', 60));
+            }
+        }
     }
 }
