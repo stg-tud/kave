7cd0032c880acf7e9c0cd7938c6205adf003a3a5
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-08-24 22:28:31 +0200

    Improve Asserts utility.

--- feedback-generator/KaVE.Commons.Tests/Utils/Assertion/AssertsTest.cs
+++ feedback-generator/KaVE.Commons.Tests/Utils/Assertion/AssertsTest.cs
@@ -21,19 +21,285 @@ namespace KaVE.Commons.Tests.Utils.Assertion
 {
     internal class AssertsTest
     {
-        [Test, ExpectedException(typeof (AssertException))]
-        public void NotSame_TheSame()
+        // NotNull
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "is null")]
+        public void NotNull_Violation()
+        {
+            Asserts.NotNull(null);
+        }
+
+        [Test]
+        public void NotNull_Ok()
+        {
+            Asserts.NotNull(new object());
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void NotNull_Violation_Formatted()
+        {
+            Asserts.NotNull(null, "a{0}", "b");
+        }
+
+        [Test]
+        public void NotNull_Ok_Formatted()
+        {
+            Asserts.NotNull(new object(), "a{0}", "b");
+        }
+
+        // Null
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "is not null")]
+        public void Null_Violation()
+        {
+            Asserts.Null(new object());
+        }
+
+        [Test]
+        public void Null_Ok()
+        {
+            Asserts.Null(null);
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void Null_Violation_Formatted()
+        {
+            Asserts.Null(new object(), "a{0}", "b");
+        }
+
+        [Test]
+        public void Null_Ok_Formatted()
+        {
+            Asserts.Null(null, "a{0}", "b");
+        }
+
+        // NotSame
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "is the same")]
+        public void NotSame_Violation()
         {
             var a = new object();
             Asserts.NotSame(a, a);
         }
 
         [Test]
-        public void NotSame_NotTheSame()
+        public void NotSame_Ok()
         {
             var a = new object();
             var b = new object();
             Asserts.NotSame(a, b);
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void NotSame_Violation_Formatted()
+        {
+            var a = new object();
+            Asserts.NotSame(a, a, "a{0}", "b");
+        }
+
+        [Test]
+        public void NotSame_Ok_Formatted()
+        {
+            var a = new object();
+            var b = new object();
+            Asserts.NotSame(a, b, "a{0}", "b");
+        }
+
+        // Same
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "is not the same")]
+        public void Same_Violation()
+        {
+            var a = new object();
+            var b = new object();
+            Asserts.Same(a, b);
+        }
+
+        [Test]
+        public void Same_Ok()
+        {
+            var a = new object();
+            Asserts.Same(a, a);
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void Same_Violation_Formatted()
+        {
+            var a = new object();
+            var b = new object();
+            Asserts.Same(a, b, "a{0}", "b");
+        }
+
+        [Test]
+        public void Same_Ok_Formatted()
+        {
+            var a = new object();
+            Asserts.Same(a, a, "a{0}", "b");
+        }
+
+        // Not
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "is true")]
+        public void Not_Violation()
+        {
+            Asserts.Not(true);
+        }
+
+        [Test]
+        public void Not_Ok()
+        {
+            Asserts.Not(false);
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void Not_Violation_Formatted()
+        {
+            Asserts.Not(true, "a{0}", "b");
+        }
+
+        [Test]
+        public void Not_Ok_Formatted()
+        {
+            Asserts.Not(false, "a{0}", "b");
+        }
+
+        // That
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "is false")]
+        public void That_Violation()
+        {
+            Asserts.That(false);
+        }
+
+        [Test]
+        public void That_Ok()
+        {
+            Asserts.That(true);
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void That_Violation_Formatted()
+        {
+            Asserts.That(false, "a{0}", "b");
+        }
+
+        [Test]
+        public void That_Ok_Formatted()
+        {
+            Asserts.That(true, "a{0}", "b");
+        }
+
+        // AreNotEqual
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "are equal: a and a")]
+        public void AreNotEqual_Violation()
+        {
+            Asserts.AreNotEqual("a", "a");
+        }
+
+        [Test]
+        public void AreNotEqual_Ok()
+        {
+            Asserts.AreNotEqual("a", "b");
+        }
+
+        // AreEqual
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "are not equal: a and b")]
+        public void AreEqual_Violation()
+        {
+            Asserts.AreEqual("a", "b");
+        }
+
+        [Test]
+        public void AreEqual_Ok()
+        {
+            Asserts.AreEqual("a", "a");
+        }
+
+        // IsLess
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "1 >= 1")]
+        public void IsLess_Violation()
+        {
+            Asserts.IsLess(1, 1);
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "2 >= 1")]
+        public void IsLess_Violation2()
+        {
+            Asserts.IsLess(2, 1);
+        }
+
+        [Test]
+        public void IsLess_Ok()
+        {
+            Asserts.IsLess(1, 2);
+        }
+
+        // IsLessOrEqual
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "2 > 1")]
+        public void IsLessOrEqual_Violation()
+        {
+            Asserts.IsLessOrEqual(2, 1);
+        }
+
+        [Test]
+        public void IsLessOrEqual_Ok()
+        {
+            Asserts.IsLessOrEqual(1, 1);
+        }
+
+        [Test]
+        public void IsLessOrEqual_Ok2()
+        {
+            Asserts.IsLessOrEqual(1, 2);
+        }
+
+        // Fail
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "unexpected condition")]
+        public void Fail()
+        {
+            Asserts.Fail();
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void Fail_Formatted()
+        {
+            Asserts.Fail("a{0}", "b");
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void Fail_Return_string()
+        {
+            // ReSharper disable once SuggestVarOrType_BuiltInTypes
+            // ReSharper disable once UnusedVariable
+            string s = Asserts.Fail<string>("a{0}", "b");
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void Fail_Return_int()
+        {
+            // ReSharper disable once SuggestVarOrType_BuiltInTypes
+            // ReSharper disable once UnusedVariable
+            int i = Asserts.Fail<int>("a{0}", "b");
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "ab")]
+        public void Fail_Return_float()
+        {
+            // ReSharper disable once SuggestVarOrType_BuiltInTypes
+            // ReSharper disable once UnusedVariable
+            float i = Asserts.Fail<float>("a{0}", "b");
+        }
+
+        [Test, ExpectedException(typeof(AssertException), ExpectedMessage = "{x}")]
+        public void ShouldIgnoreInvalidFormattingWithoutArguments()
+        {
+            // ReSharper disable once FormatStringProblem
+            Asserts.Fail("{x}");
         }
     }
 }
--- feedback-generator/KaVE.Commons/Utils/Assertion/Asserts.cs
+++ feedback-generator/KaVE.Commons/Utils/Assertion/Asserts.cs
@@ -21,42 +21,108 @@ namespace KaVE.Commons.Utils.Assertion
 {
     public static class Asserts
     {
-        [ContractAnnotation("obj:null => halt"), StringFormatMethod("message")]
-        public static void NotNull([CanBeNull] object obj, [NotNull] string message, params object[] messageArgs)
+        // do not chain the calls to improve readability of resulting stack traces!
+
+        #region reference types
+
+        [ContractAnnotation("obj:null => halt"), StringFormatMethod("text")]
+        public static void NotNull([CanBeNull] object obj, [NotNull] string text, params object[] args)
         {
-            That(obj != null, message, messageArgs);
+            if (obj == null)
+            {
+                throw GetException(text, args);
+            }
         }
 
         [ContractAnnotation("obj:null => halt")]
         public static void NotNull([CanBeNull] object obj)
         {
-            That(obj != null);
+            if (obj == null)
+            {
+                throw GetException("is null");
+            }
         }
 
-        [ContractAnnotation("obj:notnull => halt"), StringFormatMethod("message")]
-        public static void Null([CanBeNull] object obj, [NotNull] string message, params object[] messageArgs)
+        [ContractAnnotation("obj:notnull => halt"), StringFormatMethod("text")]
+        public static void Null([CanBeNull] object obj, [NotNull] string text, params object[] args)
         {
-            That(obj == null, message, messageArgs);
+            if (obj != null)
+            {
+                throw GetException(text, args);
+            }
         }
 
-        [ContractAnnotation("condition:true => halt"), StringFormatMethod("message")]
-        public static void Not(bool condition, [NotNull] string message, params object[] messageArgs)
+        [ContractAnnotation("obj:notnull => halt")]
+        public static void Null([CanBeNull] object obj)
         {
-            That(!condition, message, messageArgs);
+            if (obj != null)
+            {
+                throw GetException("is not null");
+            }
+        }
+
+        [StringFormatMethod("text")]
+        public static void NotSame(object a, object b, [NotNull] string text, params object[] args)
+        {
+            if (a == b)
+            {
+                throw GetException(text, args);
+            }
+        }
+
+        public static void NotSame(object a, object b)
+        {
+            if (a == b)
+            {
+                throw GetException("is the same");
+            }
+        }
+
+        [StringFormatMethod("text")]
+        public static void Same(object a, object b, [NotNull] string text, params object[] args)
+        {
+            if (a != b)
+            {
+                throw GetException(text, args);
+            }
+        }
+
+        public static void Same(object a, object b)
+        {
+            if (a != b)
+            {
+                throw GetException("is not the same");
+            }
+        }
+
+        #endregion
+
+        #region fails
+
+        [ContractAnnotation("condition:true => halt"), StringFormatMethod("text")]
+        public static void Not(bool condition, [NotNull] string text, params object[] args)
+        {
+            if (condition)
+            {
+                throw GetException(text, args);
+            }
         }
 
         [ContractAnnotation("condition:true => halt")]
         public static void Not(bool condition)
         {
-            That(!condition);
+            if (condition)
+            {
+                throw GetException("is true");
+            }
         }
 
-        [ContractAnnotation("condition:false => halt"), StringFormatMethod("message")]
-        public static void That(bool condition, [NotNull] string message, params object[] messageArgs)
+        [ContractAnnotation("condition:false => halt"), StringFormatMethod("text")]
+        public static void That(bool condition, [NotNull] string text, params object[] args)
         {
             if (!condition)
             {
-                Fail(message, messageArgs);
+                throw GetException(text, args);
             }
         }
 
@@ -65,32 +131,75 @@ namespace KaVE.Commons.Utils.Assertion
         {
             if (!condition)
             {
-                Fail();
+                throw GetException("is false");
             }
         }
 
-        [ContractAnnotation("=> halt"), StringFormatMethod("message")]
-        public static void Fail(string message, params object[] messageArgs)
+        #endregion
+
+        #region comparisons
+
+        public static void AreEqual(object o, object o2)
         {
-            throw new AssertException(String.Format(message, messageArgs));
+            if (!o.Equals(o2))
+            {
+                throw GetException("are not equal: {0} and {1}", o, o2);
+            }
+        }
+
+        public static void AreNotEqual(object o, object o2)
+        {
+            if (o.Equals(o2))
+            {
+                throw GetException("are equal: {0} and {1}", o, o2);
+            }
+        }
+
+        public static void IsLess<T>(T a, T b) where T : IComparable<T>
+        {
+            if (a.CompareTo(b) >= 0)
+            {
+                throw GetException("{0} >= {1}", a, b);
+            }
+        }
+
+        public static void IsLessOrEqual<T>(T a, T b) where T : IComparable<T>
+        {
+            if (a.CompareTo(b) > 0)
+            {
+                throw GetException("{0} > {1}", a, b);
+            }
+        }
+
+        // TODO provide more assertions
+
+        #endregion
+
+        #region fails
+
+        [ContractAnnotation("=> halt"), StringFormatMethod("text")]
+        public static void Fail(string text, params object[] args)
+        {
+            throw GetException(text, args);
         }
 
         [ContractAnnotation("=> halt")]
         public static void Fail()
         {
-            throw new AssertException("unexpected condition");
+            throw GetException("unexpected condition");
         }
 
-        [ContractAnnotation("=> halt"), StringFormatMethod("message")]
-        public static TR Fail<TR>(string message, params object[] messageArgs)
+        [ContractAnnotation("=> halt"), StringFormatMethod("text")]
+        public static TR Fail<TR>(string text, params object[] args)
         {
-            Fail(message, messageArgs);
-            return default(TR);
+            throw GetException(text, args);
         }
 
-        public static void NotSame(object a, object b)
+        #endregion
+
+        private static Exception GetException(string text, params object[] args)
         {
-            Not(a == b, "same objects");
+            return new AssertException(args.Length == 0 ? text : text.FormatEx(args));
         }
     }
 }
