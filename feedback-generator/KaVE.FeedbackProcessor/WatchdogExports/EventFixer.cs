c5acd703c359881406294441d6f86ba3d1e09586
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-08-26 03:11:27 +0200

    Provide an EventFixer that filters the event stream to enforce some guarantees (e.g., trigger time) and fixes legacy events (e.g., no test start).

---
+++ feedback-generator/KaVE.FeedbackProcessor/WatchdogExports/EventFixer.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections.Generic;
+using KaVE.Commons.Model.Events;
+using KaVE.Commons.Model.Events.TestRunEvents;
+
+namespace KaVE.FeedbackProcessor.WatchdogExports
+{
+    public interface IEventFixer
+    {
+        IEnumerable<IDEEvent> FixAndFilter(IEnumerable<IDEEvent> events);
+    }
+
+    public class EventFixer : IEventFixer
+    {
+        public IEnumerable<IDEEvent> FixAndFilter(IEnumerable<IDEEvent> events)
+        {
+            foreach (var e in events)
+            {
+                if (!e.TriggeredAt.HasValue)
+                {
+                    continue;
+                }
+
+                var tre = e as TestRunEvent;
+                if (tre != null)
+                {
+                    var lastEnd = e.TriggeredAt.Value;
+                    foreach (var tr in tre.Tests)
+                    {
+                        if (!tr.StartTime.HasValue)
+                        {
+                            tr.StartTime = lastEnd;
+                            lastEnd += tr.Duration;
+                        }
+                    }
+                }
+
+                yield return e;
+            }
+        }
+    }
+}
