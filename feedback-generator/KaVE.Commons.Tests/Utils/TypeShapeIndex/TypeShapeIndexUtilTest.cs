b345208fe94db4945c864e01d9abacc1ae27fa1a
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-11-01 16:51:45 +0100

    Add missing license headers.

--- feedback-generator/KaVE.Commons.Tests/Utils/TypeShapeIndex/TypeShapeIndexUtilTest.cs
+++ feedback-generator/KaVE.Commons.Tests/Utils/TypeShapeIndex/TypeShapeIndexUtilTest.cs
@@ -1,6 +1,20 @@
-﻿using KaVE.Commons.Model.Naming;
-using KaVE.Commons.Model.Naming.Types;
-using KaVE.Commons.Model.Naming.Types.Organization;
+﻿/*
+ * Copyright 2014 Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using KaVE.Commons.Model.Naming;
 using KaVE.Commons.Utils.TypeShapeIndex;
 using NUnit.Framework;
 
@@ -11,7 +25,7 @@ namespace KaVE.Commons.Tests.Utils.TypeShapeIndex
         [Test]
         public void ReturnsAssemblyNameAsFileName()
         {
-            ITypeName type = Names.Type("System.IDisposable, mscorlib, 4.0.0.0");
+            var type = Names.Type("System.IDisposable, mscorlib, 4.0.0.0");
             var assemblyFileName = TypeShapeIndexUtil.GetAssemblyFileName(type.Assembly);
             Assert.AreEqual("mscorlib-4.0.0.0", assemblyFileName);
         }
