93912b4da025349c0ede41d7806f52185f2606d2
Sebastian Proksch <development@mail.proks.ch>
2017-04-27 04:01:38 +0200

    Add missing test class.

---
+++ feedback-generator/KaVE.Commons.Tests/Model/SSTs/Impl/Visitor/LinesOfCodeVisitorTest.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Copyright 2017 Sebastian Proksch
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using NUnit.Framework;
+
+namespace KaVE.Commons.Tests.Model.SSTs.Impl.Visitor
+{
+    internal class LinesOfCodeVisitorTest
+    {
+        [Test, Ignore]
+        public void testMe()
+        {
+            // TODO @seb: add tests!!!
+            Assert.Fail("test me!");
+        }
+    }
+}
